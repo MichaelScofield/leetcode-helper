855dd8cb772a8ce1be5bf6b8ae140aca361db6b2
luo.fucong <luo.fucong@immomo.com>
2020-07-28 10:58:07 +0800

    https://leetcode.com/problems/permutations/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,38 +3,30 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        if candidates.len() < 1 {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 1 {
             return Vec::with_capacity(0);
         }
-        use std::collections::HashSet;
-        fn combination_sum(candidates: &Vec<i32>, target: i32,
-                           result: Vec<i32>, results: &mut HashSet<Vec<i32>>) {
-            for &candidate in candidates.iter() {
-                if target < candidate {
-                    continue;
-                }
-                let mut result = result.clone();
-                result.push(candidate);
-                if target == candidate {
-                    result.sort();
-                    results.insert(result);
-                } else {
-                    combination_sum(&candidates, target - candidate, result, results);
-                }
+        fn permute(nums: Vec<i32>, i: usize, rs: &mut Vec<Vec<i32>>) {
+            if i == nums.len() {
+                rs.push(nums);
+                return;
+            }
+            for j in i..nums.len() {
+                let mut nums = nums.clone();
+                nums.swap(i, j);
+                permute(nums, i + 1, rs);
             }
         }
-        let mut results = HashSet::new();
-        let result = vec![];
-        combination_sum(&candidates, target, result, &mut results);
-        results.into_iter().collect()
+        let mut rs = vec![];
+        permute(nums, 0, &mut rs);
+        rs
     }
 }
 
 fn main() {
-    println!("{:?}", Solution::combination_sum(vec![2, 3, 6, 7], 7));
-    println!("{:?}", Solution::combination_sum(vec![2, 3, 5], 8));
-    println!("{:?}", Solution::combination_sum(vec![2, 3, 5], 1));
-    println!("{:?}", Solution::combination_sum(vec![2, 3, 5], 2));
-    println!("{:?}", Solution::combination_sum(vec![2, 3, 5], 5));
+    println!("{:?}", Solution::permute(vec![1, 2, 3]));
+    println!("{:?}", Solution::permute(vec![1]));
+    println!("{:?}", Solution::permute(vec![1, 2]));
+    println!("{:?}", Solution::permute(vec![1, 2, 3, 4]));
 }
