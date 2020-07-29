db09b7237c5a7fcbb5a3811ea8760860e64d685a
luo.fucong <luo.fucong@immomo.com>
2020-07-29 14:28:27 +0800

    https://leetcode.com/problems/rotate-image/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,30 +3,73 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        if nums.len() < 1 {
-            return Vec::with_capacity(0);
-        }
-        fn permute(nums: Vec<i32>, i: usize, rs: &mut Vec<Vec<i32>>) {
-            if i == nums.len() {
-                rs.push(nums);
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        fn rotate(matrix: &mut Vec<Vec<i32>>,
+                  upper_bound: usize, lower_bound: usize, left_bound: usize, right_bound: usize) {
+            if left_bound >= right_bound {
                 return;
             }
-            for j in i..nums.len() {
-                let mut nums = nums.clone();
-                nums.swap(i, j);
-                permute(nums, i + 1, rs);
+            let width = right_bound - left_bound;
+            for i in left_bound..right_bound {
+                let x1 = std::cmp::min(i + width, right_bound);
+                let y1 = upper_bound + (width - (x1 - i));
+                let t1 = matrix[y1][x1];
+                matrix[y1][x1] = matrix[upper_bound][i];
+
+                let y2 = std::cmp::min(y1 + width, lower_bound);
+                let x2 = right_bound - (width - (y2 - y1));
+                let t2 = matrix[y2][x2];
+                matrix[y2][x2] = t1;
+
+                let x3 = std::cmp::max(x2 as i32 - width as i32, left_bound as i32) as usize;
+                let y3 = lower_bound - (width - (x2 - x3));
+                let t3 = matrix[y3][x3];
+                matrix[y3][x3] = t2;
+
+                matrix[upper_bound][i] = t3;
             }
+            rotate(matrix, upper_bound + 1, lower_bound - 1,
+                   left_bound + 1, right_bound - 1);
         }
-        let mut rs = vec![];
-        permute(nums, 0, &mut rs);
-        rs
+        rotate(matrix, 0, matrix.len() - 1,
+               0, matrix.len() - 1);
     }
 }
 
+fn print_matrix(matrix: &Vec<Vec<i32>>) {
+    for row in matrix.iter() {
+        println!("{:?}", row);
+    }
+    println!();
+}
+
 fn main() {
-    println!("{:?}", Solution::permute(vec![1, 2, 3]));
-    println!("{:?}", Solution::permute(vec![1]));
-    println!("{:?}", Solution::permute(vec![1, 2]));
-    println!("{:?}", Solution::permute(vec![1, 2, 3, 4]));
+    let matrix = &mut vec![
+        vec![1, 2],
+        vec![3, 4]];
+    Solution::rotate(matrix);
+    print_matrix(matrix);
+
+    let matrix = &mut vec![
+        vec![1, 2, 3],
+        vec![4, 5, 6],
+        vec![7, 8, 9]];
+    Solution::rotate(matrix);
+    print_matrix(matrix);
+
+    let matrix = &mut vec![
+        vec![5, 1, 9, 11],
+        vec![2, 4, 8, 10],
+        vec![13, 3, 6, 7],
+        vec![15, 14, 12, 16]];
+    Solution::rotate(matrix);
+    print_matrix(matrix);
+
+    let matrix = &mut vec![
+        vec![1, 2, 3, 4],
+        vec![5, 6, 7, 8],
+        vec![9, 10, 11, 12],
+        vec![13, 14, 15, 16]];
+    Solution::rotate(matrix);
+    print_matrix(matrix);
 }
