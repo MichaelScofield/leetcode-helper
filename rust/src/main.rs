7cec0a582ce975ba53988fa23a12d37345ba2cca
luo.fucong <luo.fucong@immomo.com>
2020-07-31 13:46:38 +0800

    https://leetcode.com/problems/merge-intervals/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,30 +3,35 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn can_jump(nums: Vec<i32>) -> bool {
-        if nums.len() == 1 {
-            return true;
+    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let len = intervals.len();
+        if len == 0 {
+            return Vec::with_capacity(0);
         }
-        fn can_jump(nums: &[usize]) -> bool {
-            if nums[0] >= nums.len() - 1 {
-                return true;
+        let intervals = &mut { intervals };
+        intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+        let mut merged = vec![];
+        let mut last_merge = vec![intervals[0][0], intervals[0][1]];
+        for i in 1..len {
+            let interval = &intervals[i];
+            if interval[0] <= last_merge[1] {
+                last_merge = vec![last_merge[0], std::cmp::max(last_merge[1], interval[1])];
+            } else {
+                merged.push(last_merge);
+                last_merge = vec![interval[0], interval[1]];
             }
-            for j in 1..=nums[0] {
-                if can_jump(&nums[j..]) {
-                    return true;
-                }
-            }
-            false
         }
-        let nums: Vec<usize> = nums.iter().map(|&i| i as usize).collect();
-        can_jump(nums.as_slice())
+        merged.push(last_merge);
+        merged
     }
 }
 
 fn main() {
-    assert!(Solution::can_jump(vec![2, 3, 1, 1, 4]));
-    assert!(!Solution::can_jump(vec![3, 2, 1, 0, 4]));
-    assert!(!Solution::can_jump(vec![0, 2, 1, 1, 4]));
-    assert!(Solution::can_jump(vec![1]));
-    assert!(Solution::can_jump(vec![1, 2]));
+    println!("{:?}", Solution::merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]]));
+    println!("{:?}", Solution::merge(vec![vec![1, 4], vec![4, 5]]));
+    println!("{:?}", Solution::merge(vec![vec![4, 5], vec![1, 4]]));
+    println!("{:?}", Solution::merge(vec![vec![1, 4], vec![5, 6]]));
+    println!("{:?}", Solution::merge(vec![vec![5, 6], vec![1, 4]]));
+    println!("{:?}", Solution::merge(vec![vec![1, 6], vec![1, 4], vec![2, 5]]));
+    println!("{:?}", Solution::merge(vec![vec![1, 6]]));
 }
