50e304f3b1014bee192bac9a268a839d679c7402
luo.fucong <luo.fucong@immomo.com>
2020-07-31 12:32:14 +0800

    https://leetcode.com/problems/jump-game/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,39 +1,32 @@
-use crate::helper::util::to_string_vec;
-
 mod helper;
 
 struct Solution;
 
 impl Solution {
-    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        if strs.len() == 0 {
-            return Vec::with_capacity(0);
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        if nums.len() == 1 {
+            return true;
         }
-        let prime_nums = vec![
-            2, 3, 5, 7, 11,
-            13, 17, 19, 23, 29,
-            31, 37, 41, 43, 47,
-            53, 59, 61, 67, 71,
-            73, 79, 83, 89, 97,
-            101];
-        let calc_primes = |str: &str| -> u64 {
-            str.bytes().map(|c| prime_nums[(c - b'a') as usize]).product()
-        };
-        use std::collections::HashMap;
-        let mut groups = HashMap::new();
-        for str in strs {
-            let m = calc_primes(&str);
-            groups.entry(m).or_insert_with(|| Vec::new()).push(str);
+        fn can_jump(nums: &[usize]) -> bool {
+            if nums[0] >= nums.len() - 1 {
+                return true;
+            }
+            for j in 1..=nums[0] {
+                if can_jump(&nums[j..]) {
+                    return true;
+                }
+            }
+            false
         }
-        groups.into_iter().map(|e| e.1).collect()
+        let nums: Vec<usize> = nums.iter().map(|&i| i as usize).collect();
+        can_jump(nums.as_slice())
     }
 }
 
 fn main() {
-    let strs = to_string_vec(vec!["eat", "tea", "tan", "ate", "nat", "bat"]);
-    println!("{:?}", Solution::group_anagrams(strs));
-    let strs = to_string_vec(vec!["aaa", "aaa", "bb", "bb", "a", "a"]);
-    println!("{:?}", Solution::group_anagrams(strs));
-    let strs = to_string_vec(vec!["",]);
-    println!("{:?}", Solution::group_anagrams(strs));
+    assert!(Solution::can_jump(vec![2, 3, 1, 1, 4]));
+    assert!(!Solution::can_jump(vec![3, 2, 1, 0, 4]));
+    assert!(!Solution::can_jump(vec![0, 2, 1, 1, 4]));
+    assert!(Solution::can_jump(vec![1]));
+    assert!(Solution::can_jump(vec![1, 2]));
 }
