c02c9fd20bff8b55884d077dbac012956515e6eb
luo.fucong <luo.fucong@immomo.com>
2020-08-04 13:47:38 +0800

    https://leetcode.com/problems/subsets/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,51 +3,28 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn sort_colors(nums: &mut Vec<i32>) {
-        if nums.len() <= 1 {
-            return;
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 1 {
+            return Vec::with_capacity(0);
         }
-        let mut i = 0;
-        let mut j = nums.len() - 1;
-        let mut p = 0;
-        while p <= j {
-            if nums[p] == 0 {
-                nums.swap(i, p);
-                i += 1;
-                p += 1;
-            } else if nums[p] == 2 {
-                nums.swap(j, p);
-                if j == 0 {
-                    return;
-                } else {
-                    j -= 1;
-                }
-            } else {
-                p += 1;
+        fn subsets(nums: &[i32], mut result: Vec<i32>, results: &mut Vec<Vec<i32>>) {
+            results.push(result.clone());
+            for i in 0..nums.len() {
+                result.push(nums[i]);
+                subsets(&nums[i + 1..], result.clone(), results);
+                result.pop();
             }
         }
+        let mut results = vec![];
+        subsets(nums.as_slice(), vec![], &mut results);
+        results
     }
 }
 
 fn main() {
-    let mut tests = vec![
-        vec![2, 0, 2, 1, 1, 0],
-        vec![1, 1, 1, 1, 1, 1],
-        vec![0, 0, 0, 0],
-        vec![2, 2, 2, 2],
-        vec![2, 0, 1],
-        vec![1, 2, 0],
-        vec![0, 1, 2],
-        vec![0, 2, 1],
-        vec![1, 0],
-        vec![2, 0],
-        vec![0, 1],
-        vec![0, 2],
-        vec![2],
-        vec![1],
-        vec![0]];
-    for test in tests.iter_mut() {
-        Solution::sort_colors(test);
-        println!("{:?}", test);
-    }
+    println!("{:?}", Solution::subsets(vec![]));
+    println!("{:?}", Solution::subsets(vec![1]));
+    println!("{:?}", Solution::subsets(vec![1, 2]));
+    println!("{:?}", Solution::subsets(vec![1, 2, 3]));
+    println!("{:?}", Solution::subsets(vec![1, 2, 3, 4]));
 }
