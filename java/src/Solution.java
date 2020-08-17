4db3cc402431332b5ed0e80c08bdc147bb1b01da
luo.fucong <luo.fucong@immomo.com>
2020-08-17 11:31:27 +0800

    https://leetcode.com/problems/reverse-linked-list-ii/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -3,35 +3,65 @@ import java.util.List;
 
 public class Solution {
 
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null) {
             return null;
         }
-        ListNode prev = head;
-        ListNode curr = head.next;
-        head.next = null;
-        while (curr != null) {
+        assert m >= 1 && n >= m;
+        ListNode prev = null;
+        ListNode curr = head;
+        for (int i = 1; i < m; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+        ListNode reverse = reverse(curr, n - m + 1);
+        if (prev == null) {
+            return reverse;
+        }
+        prev.next = reverse;
+        return head;
+    }
+
+    ListNode reverse(ListNode node, int len) {
+        assert node != null;
+        ListNode prev = null;
+        ListNode curr = node;
+        int i = 0;
+        while (curr != null && i++ < len) {
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
+        node.next = curr;
         return prev;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        List<Integer[]> inputs = Arrays.asList(
-                new Integer[]{1, 1, 2}
-                , new Integer[]{1, 1, 2, 3, 3}
-                , new Integer[]{1}
-                , new Integer[]{1, 1}
-                , new Integer[]{1, 2, 3, 3, 4, 4, 5}
-                , new Integer[]{1, 1, 1, 2, 3}
+        class TestCase {
+            final ListNode list;
+            final int m;
+            final int n;
+
+            TestCase(ListNode list, int m, int n) {
+                this.list = list;
+                this.m = m;
+                this.n = n;
+            }
+        }
+        List<TestCase> inputs = Arrays.asList(
+                new TestCase(ListNode.from(new Integer[]{1, 2, 3, 4, 5}), 2, 4)
+                , new TestCase(ListNode.from(new Integer[]{1, 2, 3, 4, 5}), 1, 5)
+                , new TestCase(ListNode.from(new Integer[]{1, 2, 3, 4, 5}), 1, 1)
+                , new TestCase(ListNode.from(new Integer[]{1, 2, 3, 4, 5}), 3, 3)
+                , new TestCase(ListNode.from(new Integer[]{1, 2, 3, 4, 5}), 4, 5)
+                , new TestCase(ListNode.from(new Integer[]{1}), 1, 1)
+                , new TestCase(ListNode.from(new Integer[]{1, 2}), 2, 2)
+                , new TestCase(ListNode.from(new Integer[]{1, 2}), 1, 2)
         );
-        for (Integer[] vals : inputs) {
-            ListNode head = ListNode.from(vals);
-            head = solution.reverseList(head);
+        for (TestCase testCase : inputs) {
+            ListNode head = solution.reverseBetween(testCase.list, testCase.m, testCase.n);
             ListNode.printList(head);
         }
     }
