e8e599a5d4f03f4e5bcff925ed8b7fb19b08044a
luo.fucong <luo.fucong@immomo.com>
2020-08-22 16:24:22 +0800

    https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,37 +1,34 @@
 public class Solution {
 
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0) {
-            return false;
+    public String replaceSpace(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
         }
-        int m = matrix[0].length;
-        int n = matrix.length;
-        for (int i = m - 1, j = 0; i >= 0 && j < n; ) {
-            if (matrix[j][i] == target) {
-                return true;
-            }
-            if (matrix[j][i] < target) {
-                j += 1;
+        int whitespaces = (int) s.chars().filter(c -> c == ' ').count();
+        if (whitespaces < 1) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        char[] newChars = new char[chars.length - whitespaces + whitespaces * 3];
+        for (int i = chars.length - 1, j = newChars.length - 1; i >= 0; i--) {
+            if (chars[i] == ' ') {
+                newChars[j--] = '0';
+                newChars[j--] = '2';
+                newChars[j--] = '%';
             } else {
-                i -= 1;
+                newChars[j--] = chars[i];
             }
         }
-        return false;
+        return new String(newChars, 0, newChars.length);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] nums = new int[][]{
-                new int[]{1, 4, 7, 11, 15},
-                new int[]{2, 5, 8, 12, 19},
-                new int[]{3, 6, 9, 16, 22},
-                new int[]{10, 13, 14, 17, 24},
-                new int[]{18, 21, 23, 26, 30}
-        };
-        System.out.println(solution.searchMatrix(nums, 5));
-        System.out.println(solution.searchMatrix(nums, 20));
-        System.out.println(solution.searchMatrix(nums, 1));
-        System.out.println(solution.searchMatrix(nums, 30));
-        System.out.println(solution.searchMatrix(nums, 31));
+        System.out.println(solution.replaceSpace(" "));
+        System.out.println(solution.replaceSpace("a "));
+        System.out.println(solution.replaceSpace(" b"));
+        System.out.println(solution.replaceSpace("a b"));
+        System.out.println(solution.replaceSpace("happy."));
+        System.out.println(solution.replaceSpace("We are happy."));
     }
 }
