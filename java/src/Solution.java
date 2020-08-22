7c61b2dfb7aa1538fdc81e9db7d975952ce13cd0
luo.fucong <luo.fucong@immomo.com>
2020-08-22 15:05:16 +0800

    https://leetcode.com/problems/search-a-2d-matrix-ii/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,45 +1,37 @@
-import java.util.Arrays;
-
 public class Solution {
 
-    public int findDuplicate(int[] nums) {
-        if (nums == null) {
-            return -1;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
         }
-        int i = 1, j = nums.length - 1;
-        while (i != j) {
-            int mid = i + (j - i) / 2;
-            int c = countNumsInRange(nums, i, mid);
-            int expected = mid - i + 1;
-            if (c <= expected) {
-                i = mid + 1;
+        int m = matrix[0].length;
+        int n = matrix.length;
+        for (int i = m - 1, j = 0; i >= 0 && j < n; ) {
+            if (matrix[j][i] == target) {
+                return true;
+            }
+            if (matrix[j][i] < target) {
+                j += 1;
             } else {
-                j = mid;
+                i -= 1;
             }
         }
-        return i;
-    }
-
-    private int countNumsInRange(int[] nums, int i, int j) {
-        return (int) Arrays.stream(nums).filter(n -> n >= i && n <= j).count();
+        return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = new int[]{1, 3, 4, 2, 2};
-        int duplicate = solution.findDuplicate(nums);
-        System.out.println(duplicate == 2);
-
-        nums = new int[]{3, 1, 3, 4, 2};
-        duplicate = solution.findDuplicate(nums);
-        System.out.println(duplicate == 3);
-
-        nums = new int[]{2, 2, 2, 2, 2};
-        duplicate = solution.findDuplicate(nums);
-        System.out.println(duplicate == 2);
-
-        nums = new int[]{1, 1};
-        duplicate = solution.findDuplicate(nums);
-        System.out.println(duplicate == 1);
+        int[][] nums = new int[][]{
+                new int[]{1, 4, 7, 11, 15},
+                new int[]{2, 5, 8, 12, 19},
+                new int[]{3, 6, 9, 16, 22},
+                new int[]{10, 13, 14, 17, 24},
+                new int[]{18, 21, 23, 26, 30}
+        };
+        System.out.println(solution.searchMatrix(nums, 5));
+        System.out.println(solution.searchMatrix(nums, 20));
+        System.out.println(solution.searchMatrix(nums, 1));
+        System.out.println(solution.searchMatrix(nums, 30));
+        System.out.println(solution.searchMatrix(nums, 31));
     }
 }
