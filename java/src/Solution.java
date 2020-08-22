6d41f325e31e5026fd1cbf875ae06cc1179cd4f8
luo.fucong <luo.fucong@immomo.com>
2020-08-22 14:42:14 +0800

    https://leetcode.com/problems/find-the-duplicate-number/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,92 +1,45 @@
 import java.util.Arrays;
-import java.util.List;
 
 public class Solution {
 
-    public ListNode sortList(ListNode head) {
-        if (head == null) {
-            return null;
+    public int findDuplicate(int[] nums) {
+        if (nums == null) {
+            return -1;
         }
-        return sort(head, null);
-    }
-
-    static class Partition {
-        final ListNode subListHead;
-        final ListNode pivotHead;
-        final ListNode subListTail;
-
-        Partition(ListNode subListHead, ListNode pivotHead, ListNode subListTail) {
-            this.subListHead = subListHead;
-            this.pivotHead = pivotHead;
-            this.subListTail = subListTail;
-        }
-
-        @Override
-        public String toString() {
-            return "Partition{" +
-                    "subListHead=" + subListHead +
-                    ", pivotHead=" + pivotHead +
-                    ", subListTail=" + subListTail +
-                    '}';
-        }
-    }
-
-    ListNode sort(ListNode head, ListNode tailNext) {
-        if (head == tailNext || head.next == tailNext) {
-            return head;
-        }
-        Partition partition = partition(head, tailNext);
-        partition.subListTail.next = tailNext;
-        if (partition.subListHead == partition.pivotHead) {
-            partition.subListHead.next = sort(partition.pivotHead.next, tailNext);
-            return partition.subListHead;
-        } else {
-            sort(partition.pivotHead, tailNext);
-            return sort(partition.subListHead, partition.pivotHead);
-        }
-    }
-
-    Partition partition(ListNode head, ListNode tailNext) {
-        int pivot = head.val;
-        ListNode subListDummyHead = new ListNode(0, head);
-        ListNode subListHead = subListDummyHead;
-        ListNode pivotDummyHead = new ListNode(0, head);
-        ListNode pivotHead = pivotDummyHead;
-        while (head != tailNext) {
-            if (head.val < pivot) {
-                subListHead.next = head;
-                subListHead = subListHead.next;
+        int i = 1, j = nums.length - 1;
+        while (i != j) {
+            int mid = i + (j - i) / 2;
+            int c = countNumsInRange(nums, i, mid);
+            int expected = mid - i + 1;
+            if (c <= expected) {
+                i = mid + 1;
             } else {
-                pivotHead.next = head;
-                pivotHead = pivotHead.next;
+                j = mid;
             }
-            head = head.next;
         }
-        subListHead.next = pivotDummyHead.next;
-        return new Partition(subListDummyHead.next, pivotDummyHead.next, pivotHead);
+        return i;
+    }
+
+    private int countNumsInRange(int[] nums, int i, int j) {
+        return (int) Arrays.stream(nums).filter(n -> n >= i && n <= j).count();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        class TestCase {
-            final ListNode list;
+        int[] nums = new int[]{1, 3, 4, 2, 2};
+        int duplicate = solution.findDuplicate(nums);
+        System.out.println(duplicate == 2);
 
-            TestCase(Integer[] list) {
-                this.list = ListNode.from(list);
-            }
-        }
-        List<TestCase> inputs = Arrays.asList(
-                new TestCase(new Integer[]{3, 2, 1}),
-                new TestCase(new Integer[]{1, 2, 3, 4, 5}),
-                new TestCase(new Integer[]{1, 4, 3, 2, 5, 2}),
-                new TestCase(new Integer[]{-1, 5, 3, 4, 0}),
-                new TestCase(new Integer[]{4, 2, 1, 3}),
-                new TestCase(new Integer[]{1, 2}),
-                new TestCase(new Integer[]{1})
-        );
-        for (TestCase testCase : inputs) {
-            ListNode head = solution.sortList(testCase.list);
-            ListNode.printList(head);
-        }
+        nums = new int[]{3, 1, 3, 4, 2};
+        duplicate = solution.findDuplicate(nums);
+        System.out.println(duplicate == 3);
+
+        nums = new int[]{2, 2, 2, 2, 2};
+        duplicate = solution.findDuplicate(nums);
+        System.out.println(duplicate == 2);
+
+        nums = new int[]{1, 1};
+        duplicate = solution.findDuplicate(nums);
+        System.out.println(duplicate == 1);
     }
 }
