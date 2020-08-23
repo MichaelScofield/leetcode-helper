734b382f68f5edee74cd421831e8dc3696aae232
luo.fucong <luo.fucong@immomo.com>
2020-08-23 14:58:19 +0800

    https://leetcode.com/problems/powx-n/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,64 +2,43 @@ mod helper;
 
 struct Solution;
 
-struct MyQueue {
-    pub stack: Vec<i32>,
-    pub reverse_stack: Vec<i32>,
-}
-
-impl MyQueue {
-    fn new() -> Self {
-        MyQueue {
-            stack: Vec::<i32>::new(),
-            reverse_stack: Vec::<i32>::new(),
+impl Solution {
+    pub fn my_pow(x: f64, n: i32) -> f64 {
+        if n == 0 {
+            return 1.0;
         }
-    }
-
-    fn flush_stack(&mut self) {
-        while let Some(v) = self.stack.pop() {
-            self.reverse_stack.push(v);
+        if x == 0.0 {
+            return 0.0;
         }
-    }
-
-    fn push(&mut self, x: i32) {
-        self.stack.push(x)
-    }
-
-    fn pop(&mut self) -> i32 {
-        match self.reverse_stack.pop() {
-            Some(e) => { e }
-            None => {
-                self.flush_stack();
-                self.reverse_stack.pop().unwrap()
+        if n == 1 {
+            return x;
+        }
+        let m = n.abs() as usize;
+        let mut dp = vec![0.0; m + 1];
+        fn pow(x: f64, n: usize, dp: &mut Vec<f64>) -> f64 {
+            if n == 1 {
+                return x;
             }
-        }
-    }
-
-    fn peek(&mut self) -> i32 {
-        *match self.reverse_stack.last() {
-            Some(e) => { e }
-            None => {
-                self.flush_stack();
-                self.reverse_stack.last().unwrap()
+            if dp[n] != 0.0 {
+                return dp[n];
             }
+            let m = n / 2;
+            dp[m] = pow(x, m, dp);
+            if n % 2 == 1 {
+                dp[n - m] = pow(x, n - m, dp);
+            }
+            dp[m] * dp[n - m]
         }
-    }
-
-    fn empty(&self) -> bool {
-        self.reverse_stack.is_empty() && self.stack.is_empty()
+        let result = pow(x, m, &mut dp);
+        if n > 0 { result } else { 1.0 / result }
     }
 }
 
 fn main() {
-    let queue = &mut MyQueue::new();
-    println!("{}", queue.empty());
-    queue.push(1);
-    queue.push(2);
-    println!("{}", queue.peek());
-    println!("{}", queue.pop());
-    println!("{}", queue.pop());
-    println!("{}", queue.empty());
-    queue.push(3);
-    println!("{}", queue.peek());
-    println!("{}", queue.empty());
+    println!("{}", Solution::my_pow(0.00001, 2147483647));
+    println!("{}", Solution::my_pow(2.0, 10));
+    println!("{}", Solution::my_pow(2.1, 3));
+    println!("{}", Solution::my_pow(2.0, -2));
+    println!("{}", Solution::my_pow(0.0, 9));
+    println!("{}", Solution::my_pow(1.0, 8));
 }
