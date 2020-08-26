51bf85a5005845f302d9ddf0c6afd6a929e94b9b
luo.fucong <luo.fucong@immomo.com>
2020-08-26 13:55:23 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn max_profit(prices: Vec<i32>) -> i32 {
+    pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
         if prices.len() == 0 {
             return 0;
         }
@@ -11,16 +11,14 @@ impl Solution {
         let mut dp = vec![vec![0, 0]; n + 1];
         dp[0][0] = 0;
         dp[0][1] = std::i32::MIN;
-        dp[1][0] = 0;
-        dp[1][1] = -prices[0];
-        for x in 2..n + 1 {
+        for x in 1..n + 1 {
             dp[x][0] = std::cmp::max(dp[x - 1][0], dp[x - 1][1] + prices[x - 1]);
-            dp[x][1] = std::cmp::max(dp[x - 2][0] - prices[x - 1], dp[x - 1][1]);
+            dp[x][1] = std::cmp::max(dp[x - 1][0] - prices[x - 1] - fee, dp[x - 1][1]);
         }
-        dp[n][0]
+        std::cmp::max(0, dp[n][0])
     }
 }
 
 fn main() {
-    assert_eq!(3, Solution::max_profit(vec![1, 2, 3, 0, 2]));
+    assert_eq!(8, Solution::max_profit(vec![1, 3, 2, 8, 4, 9], 2));
 }
