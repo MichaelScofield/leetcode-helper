9fdc08c7ee8513a6976f3936d7391c014be88807
luo.fucong <luo.fucong@immomo.com>
2020-08-30 15:07:41 +0800

    https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,93 +1,63 @@
-import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
-    public boolean exist(char[][] board, String word) {
-        List<SimpleImmutableEntry<Integer, Integer>> startPositions = new ArrayList<>();
-        int m = board.length;
-        int n = board[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    startPositions.add(new SimpleImmutableEntry<>(i, j));
-                }
-            }
+    int cells = 0;
+
+    public int movingCount(int m, int n, int k) {
+        if (k == 0) {
+            return 1;
         }
-        for (SimpleImmutableEntry<Integer, Integer> startPosition : startPositions) {
-            boolean[][] visited = new boolean[m][n];
-            int i = startPosition.getKey();
-            int j = startPosition.getValue();
-            visited[i][j] = true;
-            if (backtrace(board, word, 1, visited, startPosition)) {
-                return true;
-            }
-        }
-        return false;
+        boolean[][] visited = new boolean[m][n];
+        backtrace(m, n, k, visited, 0, 0);
+        return cells;
     }
 
-    boolean backtrace(char[][] board, String word,
-                      int k, boolean[][] visited, SimpleImmutableEntry<Integer, Integer> position) {
-        if (k == word.length()) {
-            return true;
-        }
-        int m = board.length;
-        int n = board[0].length;
-        char expected = word.charAt(k);
-        int i = position.getKey();
-        int j = position.getValue();
-
+    void backtrace(int m, int n, int k, boolean[][] visited, int i, int j) {
         // going Right:
-        if (j < n - 1 && board[i][j + 1] == expected && !visited[i][j + 1]) {
+        if (j < n - 1 && !visited[i][j + 1] && canEnter(i, j + 1, k)) {
             visited[i][j + 1] = true;
-            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i, j + 1))) {
-                return true;
-            }
-            visited[i][j + 1] = false;
+            cells += 1;
+            backtrace(m, n, k, visited, i, j + 1);
         }
 
         // going Down:
-        if (i < m - 1 && board[i + 1][j] == expected && !visited[i + 1][j]) {
+        if (i < m - 1 && !visited[i + 1][j] && canEnter(i + 1, j, k)) {
             visited[i + 1][j] = true;
-            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i + 1, j))) {
-                return true;
-            }
-            visited[i + 1][j] = false;
+            cells += 1;
+            backtrace(m, n, k, visited, i + 1, j);
         }
 
         // going Left:
-        if (j > 0 && board[i][j - 1] == expected && !visited[i][j - 1]) {
+        if (j > 0 && !visited[i][j - 1] && canEnter(i, j - 1, k)) {
             visited[i][j - 1] = true;
-            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i, j - 1))) {
-                return true;
-            }
-            visited[i][j - 1] = false;
+            cells += 1;
+            backtrace(m, n, k, visited, i, j - 1);
         }
 
         // going Up:
-        if (i > 0 && board[i - 1][j] == expected && !visited[i - 1][j]) {
+        if (i > 0 && !visited[i - 1][j] && canEnter(i - 1, j, k)) {
             visited[i - 1][j] = true;
-            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i - 1, j))) {
-                return true;
-            }
-            visited[i - 1][j] = false;
+            cells += 1;
+            backtrace(m, n, k, visited, i - 1, j);
         }
-        return false;
+    }
+
+    boolean canEnter(int i, int j, int k) {
+        int x = 0;
+        while (i > 0) {
+            x += i % 10;
+            i /= 10;
+        }
+        int y = 0;
+        while (j > 0) {
+            y += j % 10;
+            j /= 10;
+        }
+        return x + y <= k;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        char[][] board = new char[][]{
-                new char[]{'A', 'B', 'C', 'E'},
-                new char[]{'S', 'F', 'C', 'S'},
-                new char[]{'A', 'D', 'E', 'E'}
-        };
-        System.out.println(solution.exist(board, "ABCCED"));
-        System.out.println(solution.exist(board, "SEE"));
-        System.out.println(solution.exist(board, "ABCB"));
-        System.out.println(solution.exist(board, "A"));
-        System.out.println(solution.exist(board, "B"));
-        System.out.println(solution.exist(board, "X"));
+        System.out.println(solution.movingCount(2, 3, 1));
+        System.out.println(solution.movingCount(3, 1, 0));
     }
 }
