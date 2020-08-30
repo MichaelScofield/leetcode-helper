66f40a30519def881a7d0a07f903c6a803c7bc9d
luo.fucong <luo.fucong@immomo.com>
2020-08-30 14:44:36 +0800

    https://leetcode.com/problems/word-search/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,35 +1,93 @@
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || inorder == null
-                || preorder.length != inorder.length
-                || preorder.length < 1) {
-            return null;
-        }
-        return buildTree(preorder, 0, preorder.length, inorder, 0, inorder.length);
-    }
-
-    TreeNode buildTree(int[] preorder, int p, int q, int[] inorder, int x, int y) {
-        if (p >= q || x >= y) {
-            return null;
-        }
-        TreeNode root = new TreeNode(preorder[p]);
-        int i;
-        for (i = x; i < y; i++) {
-            if (inorder[i] == preorder[p]) {
-                break;
+    public boolean exist(char[][] board, String word) {
+        List<SimpleImmutableEntry<Integer, Integer>> startPositions = new ArrayList<>();
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    startPositions.add(new SimpleImmutableEntry<>(i, j));
+                }
             }
         }
-        root.left = buildTree(preorder, p + 1, p + 1 + (i - x), inorder, x, i);
-        root.right = buildTree(preorder, p + 1 + (i - x), q, inorder, i + 1, y);
-        return root;
+        for (SimpleImmutableEntry<Integer, Integer> startPosition : startPositions) {
+            boolean[][] visited = new boolean[m][n];
+            int i = startPosition.getKey();
+            int j = startPosition.getValue();
+            visited[i][j] = true;
+            if (backtrace(board, word, 1, visited, startPosition)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean backtrace(char[][] board, String word,
+                      int k, boolean[][] visited, SimpleImmutableEntry<Integer, Integer> position) {
+        if (k == word.length()) {
+            return true;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        char expected = word.charAt(k);
+        int i = position.getKey();
+        int j = position.getValue();
+
+        // going Right:
+        if (j < n - 1 && board[i][j + 1] == expected && !visited[i][j + 1]) {
+            visited[i][j + 1] = true;
+            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i, j + 1))) {
+                return true;
+            }
+            visited[i][j + 1] = false;
+        }
+
+        // going Down:
+        if (i < m - 1 && board[i + 1][j] == expected && !visited[i + 1][j]) {
+            visited[i + 1][j] = true;
+            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i + 1, j))) {
+                return true;
+            }
+            visited[i + 1][j] = false;
+        }
+
+        // going Left:
+        if (j > 0 && board[i][j - 1] == expected && !visited[i][j - 1]) {
+            visited[i][j - 1] = true;
+            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i, j - 1))) {
+                return true;
+            }
+            visited[i][j - 1] = false;
+        }
+
+        // going Up:
+        if (i > 0 && board[i - 1][j] == expected && !visited[i - 1][j]) {
+            visited[i - 1][j] = true;
+            if (backtrace(board, word, k + 1, visited, new SimpleImmutableEntry<>(i - 1, j))) {
+                return true;
+            }
+            visited[i - 1][j] = false;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] preorder = new int[]{3, 9, 20, 15, 7};
-        int[] inorder = new int[]{9, 3, 15, 20, 7};
-        TreeNode root = solution.buildTree(preorder, inorder);
-        TreeNode.printTree(root);
+        char[][] board = new char[][]{
+                new char[]{'A', 'B', 'C', 'E'},
+                new char[]{'S', 'F', 'C', 'S'},
+                new char[]{'A', 'D', 'E', 'E'}
+        };
+        System.out.println(solution.exist(board, "ABCCED"));
+        System.out.println(solution.exist(board, "SEE"));
+        System.out.println(solution.exist(board, "ABCB"));
+        System.out.println(solution.exist(board, "A"));
+        System.out.println(solution.exist(board, "B"));
+        System.out.println(solution.exist(board, "X"));
     }
 }
