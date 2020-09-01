2232ec36bea6d719b5a055de45cb0d906089ce42
luo.fucong <luo.fucong@immomo.com>
2020-09-01 11:35:47 +0800

    https://leetcode.com/problems/maximum-subarray/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,44 +3,24 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn find_target_sum_ways(nums: Vec<i32>, s: i32) -> i32 {
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
         if nums.len() == 0 {
             return 0;
         }
         let n = nums.len();
-        let sum = nums.iter().sum::<i32>() as usize;
-
-        let s = s.abs() as usize;
-        if sum < s {
-            return 0;
-        }
-
-        // State transition equation:
-        // dp[i][j] = dp[i - 1][|j - nums[i]|] + dp[i - 1][j + nums[i]]
-        // Why "|j - nums[i]|"?
-        // Because for every solution,
-        // Σ(nums) = -s <=> -Σ(nums) = s,
-        // we can omit the storage for negative sum by dp[-sum] = dp[sum].
-        let mut dp = vec![vec![0; sum + 1]; n + 1];
-        dp[0][0] = 1;
+        let mut dp = vec![0; n];
+        let mut max = std::i32::MIN;
         for i in 1..=n {
-            let num = nums[i - 1] as usize;
-            for j in 0..=sum {
-                if j >= num {
-                    dp[i][j] += dp[i - 1][j - num];
-                } else {
-                    dp[i][j] += dp[i - 1][num - j];
-                }
-                if j + num <= sum {
-                    dp[i][j] += dp[i - 1][j + num];
-                }
+            for j in 0..=n - i {
+                dp[j] = dp[j] + nums[j + i - 1];
+                max = std::cmp::max(max, dp[j]);
             }
         }
-        dp[n][s]
+        max
     }
 }
 
 fn main() {
-    assert_eq!(5, Solution::find_target_sum_ways(vec![1, 1, 1, 1, 1], 3));
-    assert_eq!(0, Solution::find_target_sum_ways(vec![1], 2));
+    assert_eq!(6, Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+    assert_eq!(-1, Solution::max_sub_array(vec![-1]));
 }
