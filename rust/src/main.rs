db7be3ad8a99b5d8e51554faf3fb859b55d69c9e
luo.fucong <luo.fucong@immomo.com>
2020-09-02 11:09:55 +0800

    https://leetcode.com/problems/edit-distance/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,46 +3,41 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn my_pow(x: f64, n: i32) -> f64 {
+    pub fn min_distance(word1: String, word2: String) -> i32 {
+        let m = word1.len();
+        let n = word2.len();
+        if m == 0 {
+            return n as i32;
+        }
         if n == 0 {
-            return 1.0;
+            return m as i32;
         }
-        if x == 0.0 {
-            assert!(n > 0);
-            return 0.0;
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+        for i in 0..=m {
+            dp[i][0] = i;
         }
-        let m = n.abs() as u32;
-        use std::collections::HashMap;
-        let mut memo = HashMap::new();
-        fn pow(x: f64, i: u32, memo: &mut HashMap<u32, f64>) -> f64 {
-            if i == 0 {
-                return 1.0;
-            }
-            if i == 1 {
-                return x;
-            }
-            if let Some(&value) = memo.get(&i) {
-                return value;
-            }
-            let a = pow(x, i / 2, memo);
-            memo.insert(i / 2, a);
-            if i % 2 == 0 {
-                a * a
-            } else {
-                memo.insert(i / 2 + 1, a * x);
-                a * a * x
+        for i in 0..=n {
+            dp[0][i] = i;
+        }
+        let word1 = word1.as_bytes();
+        let word2 = word2.as_bytes();
+        for i in 1..=m {
+            for j in 1..=n {
+                if word1[i - 1] == word2[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    let del = dp[i - 1][j] + 1;
+                    let insert = dp[i][j - 1] + 1;
+                    let replace = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = std::cmp::min(std::cmp::min(del, insert), replace);
+                }
             }
         }
-        let result = pow(x, m, &mut memo);
-        if n > 0 { result } else { 1.0 / result }
+        dp[m][n] as i32
     }
 }
 
 fn main() {
-    println!("{}", Solution::my_pow(0.00001, 2147483647));
-    println!("{}", Solution::my_pow(2.0, 10));
-    println!("{}", Solution::my_pow(2.1, 3));
-    println!("{}", Solution::my_pow(2.0, -2));
-    println!("{}", Solution::my_pow(0.0, 9));
-    println!("{}", Solution::my_pow(1.0, 8));
+    assert_eq!(3, Solution::min_distance("horse".to_string(), "ros".to_string()));
+    assert_eq!(5, Solution::min_distance("intention".to_string(), "execution".to_string()));
 }
