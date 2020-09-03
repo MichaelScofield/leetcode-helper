7530c0bda4aeaea3ceb26e4eb8715755dcd6a317
luo.fucong <luo.fucong@immomo.com>
2020-09-03 14:08:45 +0800

    https://leetcode.com/problems/longest-common-subsequence/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,41 +3,28 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn min_distance(word1: String, word2: String) -> i32 {
-        let m = word1.len();
-        let n = word2.len();
-        if m == 0 {
-            return n as i32;
-        }
-        if n == 0 {
-            return m as i32;
-        }
+    pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        assert!(text1.len() > 0 && text2.len() > 0);
+        let text1 = text1.as_bytes();
+        let text2 = text2.as_bytes();
+        let m = text1.len();
+        let n = text2.len();
         let mut dp = vec![vec![0; n + 1]; m + 1];
-        for i in 0..=m {
-            dp[i][0] = i;
-        }
-        for i in 0..=n {
-            dp[0][i] = i;
-        }
-        let word1 = word1.as_bytes();
-        let word2 = word2.as_bytes();
         for i in 1..=m {
             for j in 1..=n {
-                if word1[i - 1] == word2[j - 1] {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    let del = dp[i - 1][j] + 1;
-                    let insert = dp[i][j - 1] + 1;
-                    let replace = dp[i - 1][j - 1] + 1;
-                    dp[i][j] = std::cmp::min(std::cmp::min(del, insert), replace);
-                }
+                dp[i][j] = std::cmp::max(
+                    dp[i - 1][j - 1] + if text1[i - 1] == text2[j - 1] { 1 } else { 0 },
+                    std::cmp::max(dp[i][j - 1], dp[i - 1][j]));
             }
         }
-        dp[m][n] as i32
+        dp[m][n]
     }
 }
 
 fn main() {
-    assert_eq!(3, Solution::min_distance("horse".to_string(), "ros".to_string()));
-    assert_eq!(5, Solution::min_distance("intention".to_string(), "execution".to_string()));
+    assert_eq!(3, Solution::longest_common_subsequence("abcde".to_string(), "ace".to_string()));
+    assert_eq!(2, Solution::longest_common_subsequence("abcde".to_string(), "aec".to_string()));
+    assert_eq!(3, Solution::longest_common_subsequence("abc".to_string(), "abc".to_string()));
+    assert_eq!(0, Solution::longest_common_subsequence("abc".to_string(), "def".to_string()));
+    assert_eq!(1, Solution::longest_common_subsequence("psnw".to_string(), "vozsh".to_string()));
 }
