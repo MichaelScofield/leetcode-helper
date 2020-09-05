a7dfb102278861c5b0e773313e810269d1bd0b2e
luo.fucong <luo.fucong@immomo.com>
2020-09-05 11:39:17 +0800

    https://leetcode.com/problems/regular-expression-matching/ (WA)

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,30 +1,53 @@
 public class Solution {
 
-    public int integerBreak(int n) {
-        assert n > 0;
-        if (n == 1 || n == 2) {
-            return 1;
+    public boolean isMatch(String s, String p) {
+        if (s == null && p == null) {
+            return true;
         }
-        if (n == 3) {
-            return 2;
+        if (s == null || p == null) {
+            return false;
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        for (int i = 4; i <= n; i++) {
-            int max = 0;
-            for (int j = 0; j <= i; j++) {
-                max = Math.max(max, dp[j] * dp[i - j]);
+        int m = s.length();
+        int n = p.length();
+        if (m == 0 && n == 0) {
+            return true;
+        }
+        boolean[][] dp = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean match;
+                if (i == 0 && j == 0) {
+                    match = true;
+                } else if (i == 0 || j == 0) {
+                    match = false;
+                } else {
+                    char cs = s.charAt(i);
+                    char cp = p.charAt(j);
+                    match = dp[i - 1][j - 1] & (cs == cp || cp == '.');
+                    if (!match && cp == '*') {
+                        char last = p.charAt(j - 1);
+                        match = dp[i - 1][j - 1] & (cs == last || last == '.');
+                        if (!match && j >= 2) {
+                            match = dp[i][j - 2];
+                        }
+                    }
+                }
+                dp[i][j] = match;
             }
-            dp[i] = max;
         }
-        return dp[n];
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.integerBreak(2) == 1);
-        System.out.println(solution.integerBreak(10) == 36);
+        System.out.println(!solution.isMatch("aa", "a"));
+        System.out.println(solution.isMatch("aa", "a*"));
+        System.out.println(solution.isMatch("ab", ".*"));
+        System.out.println(solution.isMatch("aab", "c*a*b"));
+        System.out.println(!solution.isMatch("mississippi", "mis*is*p*."));
+        System.out.println(solution.isMatch("aaa", "a*a"));
+        System.out.println(solution.isMatch("aaa", "a*aa"));
+        System.out.println(solution.isMatch("aaa", "a*aaa"));
+        System.out.println(!solution.isMatch("aaa", "a*aaaa"));
     }
 }
