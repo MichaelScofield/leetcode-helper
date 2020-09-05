4bd1091b54189b7896b5d73ca48ad35dc67d83e2
luo.fucong <luo.fucong@immomo.com>
2020-09-05 11:52:11 +0800

    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,53 +1,52 @@
 public class Solution {
 
-    public boolean isMatch(String s, String p) {
-        if (s == null && p == null) {
-            return true;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
         }
-        if (s == null || p == null) {
-            return false;
+        if (n <= 0) {
+            return head;
         }
-        int m = s.length();
-        int n = p.length();
-        if (m == 0 && n == 0) {
-            return true;
-        }
-        boolean[][] dp = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean match;
-                if (i == 0 && j == 0) {
-                    match = true;
-                } else if (i == 0 || j == 0) {
-                    match = false;
-                } else {
-                    char cs = s.charAt(i);
-                    char cp = p.charAt(j);
-                    match = dp[i - 1][j - 1] & (cs == cp || cp == '.');
-                    if (!match && cp == '*') {
-                        char last = p.charAt(j - 1);
-                        match = dp[i - 1][j - 1] & (cs == last || last == '.');
-                        if (!match && j >= 2) {
-                            match = dp[i][j - 2];
-                        }
-                    }
-                }
-                dp[i][j] = match;
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n - 1; i++) {
+            if (fast.next != null) {
+                fast = fast.next;
+            } else {
+                return head;
             }
         }
-        return dp[m - 1][n - 1];
+        ListNode prev = null;
+        while (fast.next != null) {
+            fast = fast.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        if (prev == null) {
+            return head.next;
+        } else {
+            prev.next = slow.next;
+            return head;
+        }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(!solution.isMatch("aa", "a"));
-        System.out.println(solution.isMatch("aa", "a*"));
-        System.out.println(solution.isMatch("ab", ".*"));
-        System.out.println(solution.isMatch("aab", "c*a*b"));
-        System.out.println(!solution.isMatch("mississippi", "mis*is*p*."));
-        System.out.println(solution.isMatch("aaa", "a*a"));
-        System.out.println(solution.isMatch("aaa", "a*aa"));
-        System.out.println(solution.isMatch("aaa", "a*aaa"));
-        System.out.println(!solution.isMatch("aaa", "a*aaaa"));
+        ListNode head;
+
+        head = ListNode.from(new Integer[]{1, 2, 3, 4, 5});
+        head = solution.removeNthFromEnd(head, 2);
+        ListNode.printList(head);
+
+        head = ListNode.from(new Integer[]{1, 2, 3, 4, 5});
+        head = solution.removeNthFromEnd(head, 1);
+        ListNode.printList(head);
+
+        head = ListNode.from(new Integer[]{1, 2, 3, 4, 5});
+        head = solution.removeNthFromEnd(head, 5);
+        ListNode.printList(head);
+
+        head = ListNode.from(new Integer[]{1, 2, 3, 4, 5});
+        head = solution.removeNthFromEnd(head, 6);
+        ListNode.printList(head);
     }
 }
