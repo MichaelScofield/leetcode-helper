18f3f199b93a9a98f3c1fedb85083312fd2f92e1
luo.fucong <luo.fucong@immomo.com>
2020-09-06 16:25:44 +0800

    https://leetcode.com/problems/min-stack/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,89 +1,56 @@
 mod helper;
 
-struct Solution;
+struct MinStack {
+    data_stack: Vec<i32>,
+    min_stack: Vec<i32>,
+}
 
-impl Solution {
-    pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        let m = matrix.len();
-        if m == 0 {
-            return Vec::with_capacity(0);
+impl MinStack {
+    fn new() -> Self {
+        MinStack {
+            data_stack: Vec::new(),
+            min_stack: Vec::new(),
         }
-        let n = matrix[0].len();
-        if n == 0 {
-            return Vec::with_capacity(0);
-        }
-        let mut arr = Vec::with_capacity(m * n);
-        let rings = std::cmp::min(m / 2 + m % 2, n / 2 + n % 2);
-        for i in 0..rings {
-            let left_bound = i;
-            let right_bound = n - i;
-            let upper_bound = i;
-            let lower_bound = m - i;
-            let mut x = i;
-            let mut y = i;
-            while y < right_bound {
-                arr.push(matrix[x][y]);
-                if y + 1 == right_bound {
-                    break;
-                }
-                y += 1;
-            }
+    }
 
-            if x + 1 == lower_bound {
-                continue;
-            }
-            x += 1;
-            while x < lower_bound {
-                arr.push(matrix[x][y]);
-                if x + 1 == lower_bound {
-                    break;
-                }
-                x += 1;
-            }
-
-            if y == left_bound {
-                continue;
-            }
-            y -= 1;
-            while y >= left_bound {
-                arr.push(matrix[x][y]);
-                if y == left_bound {
-                    break;
-                }
-                y -= 1;
-            }
-
-            x -= 1;
-            while x > upper_bound {
-                arr.push(matrix[x][y]);
-                x -= 1
+    fn push(&mut self, x: i32) {
+        self.data_stack.push(x);
+        if let Some(&min) = self.min_stack.last() {
+            if min < x {
+                self.min_stack.push(min);
+                return;
             }
         }
-        arr
+        self.min_stack.push(x);
+    }
+
+    fn pop(&mut self) {
+        self.data_stack.pop();
+        self.min_stack.pop();
+    }
+
+    fn top(&mut self) -> i32 {
+        if let Some(&x) = self.data_stack.last() {
+            return x;
+        }
+        panic!("Illegal State!")
+    }
+
+    fn get_min(&self) -> i32 {
+        if let Some(&min) = self.min_stack.last() {
+            return min;
+        }
+        panic!("Illegal State!")
     }
 }
 
 fn main() {
-    let matrix = vec![
-        vec![1, 2, 3],
-        vec![4, 5, 6],
-        vec![7, 8, 9]];
-    assert_eq!(vec![1, 2, 3, 6, 9, 8, 7, 4, 5], Solution::spiral_order(matrix));
-    let matrix = vec![
-        vec![1, 2, 3, 4],
-        vec![5, 6, 7, 8],
-        vec![9, 10, 11, 12]];
-    assert_eq!(vec![1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7], Solution::spiral_order(matrix));
-    let matrix = vec![
-        vec![1, 2, 3],
-        vec![4, 5, 6],
-        vec![7, 8, 9],
-        vec![10, 11, 12]];
-    assert_eq!(vec![1, 2, 3, 6, 9, 12, 11, 10, 7, 4, 5, 8], Solution::spiral_order(matrix));
-    let matrix = vec![vec![1, 2]];
-    assert_eq!(vec![1, 2], Solution::spiral_order(matrix));
-    let matrix = vec![vec![1], vec![2]];
-    assert_eq!(vec![1, 2], Solution::spiral_order(matrix));
-    let matrix = vec![vec![1]];
-    assert_eq!(vec![1], Solution::spiral_order(matrix));
+    let min_stack = &mut MinStack::new();
+    min_stack.push(-2);
+    min_stack.push(0);
+    min_stack.push(-3);
+    assert_eq!(-3, min_stack.get_min());
+    min_stack.pop();
+    assert_eq!(0, min_stack.top());
+    assert_eq!(-2, min_stack.get_min());
 }
