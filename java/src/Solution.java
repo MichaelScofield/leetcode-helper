a032b46e97da030c5046985924bd80b853217ff2
luo.fucong <luo.fucong@immomo.com>
2020-09-06 17:54:15 +0800

    https://leetcode.com/problems/path-sum-ii/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,38 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-    public boolean isSubStructure(TreeNode A, TreeNode B) {
-        if (A == null || B == null) {
-            return false;
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return new ArrayList<>(1);
         }
-        if (isSameLargerTree(A, B)) {
-            return true;
-        }
-        return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+        List<List<Integer>> paths = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(root.val);
+        pathSum(root, sum - root.val, path, paths);
+        return paths;
     }
 
-    boolean isSameLargerTree(TreeNode ra, TreeNode rb) {
-        if (rb == null) {
-            return true;
+    void pathSum(TreeNode root, int sum, List<Integer> path, List<List<Integer>> paths) {
+        if (sum == 0 && root.left == null && root.right == null) {
+            paths.add(new ArrayList<>(path));
+            return;
         }
-        if (ra == null) {
-            return false;
+        if (root.left != null) {
+            path.add(root.left.val);
+            pathSum(root.left, sum - root.left.val, path, paths);
+            path.remove(path.size() - 1);
         }
-        if (ra.val != rb.val) {
-            return false;
+        if (root.right != null) {
+            path.add(root.right.val);
+            pathSum(root.right, sum - root.right.val, path, paths);
+            path.remove(path.size() - 1);
         }
-        return isSameLargerTree(ra.left, rb.left) && isSameLargerTree(ra.right, rb.right);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        TreeNode A, B;
+        TreeNode root;
 
-        A = TreeNode.createTree(new Integer[]{1, 2, 3});
-        B = TreeNode.createTree(new Integer[]{3, 1, null});
-        System.out.println(!solution.isSubStructure(A, B));
+        root = TreeNode.createTree(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1});
+        System.out.println(solution.pathSum(root, 22));
 
-        A = TreeNode.createTree(new Integer[]{3, 4, 5, 1, 2});
-        B = TreeNode.createTree(new Integer[]{4, 1, null});
-        System.out.println(solution.isSubStructure(A, B));
+        root = TreeNode.createTree(new Integer[]{5});
+        System.out.println(solution.pathSum(root, 22));
+
+        root = TreeNode.createTree(new Integer[]{5});
+        System.out.println(solution.pathSum(root, 5));
     }
 }
