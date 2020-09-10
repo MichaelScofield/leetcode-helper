cca7d6bbab960ae8170983cef50018247b1e53f0
luo.fucong <luo.fucong@immomo.com>
2020-09-10 23:04:36 +0800

    https://leetcode.com/problems/serialize-and-deserialize-binary-tree/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,50 +1,55 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Solution {
 
-    TreeNode lastNode;
-
-    public TreeNode treeToDoublyList(TreeNode root) {
+    public String serialize(TreeNode root) {
         if (root == null) {
-            return null;
+            return "";
         }
-        TreeNode head = root;
-        TreeNode tail = root;
-        convert(root);
-        while (head.left != null) {
-            head = head.left;
-        }
-        while (tail.right != null) {
-            tail = tail.right;
-        }
-        head.left = tail;
-        tail.right = head;
-        return head;
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
     }
 
-    void convert(TreeNode node) {
-        if (node.left != null) {
-            convert(node.left);
+    void serialize(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("x ");
+            return;
         }
-        if (lastNode != null) {
-            node.left = lastNode;
-            lastNode.right = node;
+        sb.append(node.val).append(" ");
+        serialize(node.left, sb);
+        serialize(node.right, sb);
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty()) {
+            return null;
         }
-        lastNode = node;
-        if (node.right != null) {
-            convert(node.right);
+        LinkedList<String> vals = Arrays.stream(data.trim().split(" ")).collect(LinkedList::new, LinkedList::offer, LinkedList::addAll);
+        return deserialize(vals);
+    }
+
+    TreeNode deserialize(LinkedList<String> vals) {
+        if (vals.size() == 0) {
+            return null;
         }
+        String val = vals.poll();
+        if (val.equals("x")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserialize(vals);
+        node.right = deserialize(vals);
+        return node;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        TreeNode root = TreeNode.createTree(new Integer[]{4, 2, 5, 1, 3});
-        TreeNode head = solution.treeToDoublyList(root);
-        TreeNode last = head.left;
-        TreeNode curr = head;
-        do {
-            System.out.println(curr.val);
-            System.out.println(curr.left == last);
-            last = curr;
-            curr = curr.right;
-        } while (last.right != head);
+        TreeNode root = TreeNode.createTree(new Integer[]{1, 2, 3, null, null, 4, 5});
+        String data = solution.serialize(root);
+        System.out.println(data);
+        root = solution.deserialize(data);
+        TreeNode.printTree(root);
     }
 }
