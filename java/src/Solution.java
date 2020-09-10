b9cc22aea23657a43956026fe3bd0bd42f8ff4a5
luo.fucong <luo.fucong@immomo.com>
2020-09-10 12:52:08 +0800

    https://leetcode.com/problems/search-in-a-binary-search-tree/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,67 +1,49 @@
 public class Solution {
 
-    public TreeNode deleteNode(TreeNode root, int key) {
+    public TreeNode searchBST(TreeNode root, int val) {
         if (root == null) {
             return null;
         }
-        if (root.val == key) {
-            if (root.left == null && root.right == null) {
-                return null;
-            }
-            if (root.left != null && root.right != null) {
-                TreeNode min = root.right;
-                while (min.left != null) {
-                    min = min.left;
-                }
-                root.val = min.val;
-                root.right = deleteNode(root.right, min.val);
-                return root;
-            }
-            return root.left != null ? root.left : root.right;
-        } else if (root.val < key) {
-            root.right = deleteNode(root.right, key);
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
         } else {
-            root.left = deleteNode(root.left, key);
+            return searchBST(root.left, val);
         }
-        return root;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         TreeNode root;
 
-        root = TreeNode.createTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
-        root = solution.deleteNode(root, 3);
+        root = TreeNode.createTree(new Integer[]{4, 2, 7, 1, 3});
+        root = solution.searchBST(root, 2);
         TreeNode.printTree(root);
         System.out.println();
 
-        root = TreeNode.createTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
-        root = solution.deleteNode(root, 0);
+        root = TreeNode.createTree(new Integer[]{4, 2, 7, 1, 3});
+        root = solution.searchBST(root, 0);
         TreeNode.printTree(root);
         System.out.println();
 
         root = TreeNode.createTree(new Integer[]{});
-        root = solution.deleteNode(root, 0);
+        root = solution.searchBST(root, 0);
         TreeNode.printTree(root);
         System.out.println();
 
-        root = TreeNode.createTree(new Integer[]{5, 3, 6, 2, 4, null, 7});
-        root = solution.deleteNode(root, 5);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{50, 30, 70, null, 40, 60, 80});
-        root = solution.deleteNode(root, 50);
+        root = TreeNode.createTree(new Integer[]{18, 2, 22, null, null, null, 63, null, 84, null, null});
+        root = solution.searchBST(root, 63);
         TreeNode.printTree(root);
         System.out.println();
 
         root = TreeNode.createTree(new Integer[]{5});
-        root = solution.deleteNode(root, 5);
+        root = solution.searchBST(root, 5);
         TreeNode.printTree(root);
         System.out.println();
 
         root = TreeNode.createTree(new Integer[]{1, null, 2, null, 3});
-        root = solution.deleteNode(root, 2);
+        root = solution.searchBST(root, 2);
         TreeNode.printTree(root);
         System.out.println();
     }
