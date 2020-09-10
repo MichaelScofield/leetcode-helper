70febd0b24ad9b874d4e97180bf8b05fadbc30a7
luo.fucong <luo.fucong@immomo.com>
2020-09-10 22:34:48 +0800

    https://leetcode-cn.com/problems/er-cha-sou-suo-shu-yu-shuang-xiang-lian-biao-lcof/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,50 +1,50 @@
 public class Solution {
 
-    public TreeNode searchBST(TreeNode root, int val) {
+    TreeNode lastNode;
+
+    public TreeNode treeToDoublyList(TreeNode root) {
         if (root == null) {
             return null;
         }
-        if (root.val == val) {
-            return root;
-        } else if (root.val < val) {
-            return searchBST(root.right, val);
-        } else {
-            return searchBST(root.left, val);
+        TreeNode head = root;
+        TreeNode tail = root;
+        convert(root);
+        while (head.left != null) {
+            head = head.left;
+        }
+        while (tail.right != null) {
+            tail = tail.right;
+        }
+        head.left = tail;
+        tail.right = head;
+        return head;
+    }
+
+    void convert(TreeNode node) {
+        if (node.left != null) {
+            convert(node.left);
+        }
+        if (lastNode != null) {
+            node.left = lastNode;
+            lastNode.right = node;
+        }
+        lastNode = node;
+        if (node.right != null) {
+            convert(node.right);
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        TreeNode root;
-
-        root = TreeNode.createTree(new Integer[]{4, 2, 7, 1, 3});
-        root = solution.searchBST(root, 2);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{4, 2, 7, 1, 3});
-        root = solution.searchBST(root, 0);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{});
-        root = solution.searchBST(root, 0);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{18, 2, 22, null, null, null, 63, null, 84, null, null});
-        root = solution.searchBST(root, 63);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{5});
-        root = solution.searchBST(root, 5);
-        TreeNode.printTree(root);
-        System.out.println();
-
-        root = TreeNode.createTree(new Integer[]{1, null, 2, null, 3});
-        root = solution.searchBST(root, 2);
-        TreeNode.printTree(root);
-        System.out.println();
+        TreeNode root = TreeNode.createTree(new Integer[]{4, 2, 5, 1, 3});
+        TreeNode head = solution.treeToDoublyList(root);
+        TreeNode last = head.left;
+        TreeNode curr = head;
+        do {
+            System.out.println(curr.val);
+            System.out.println(curr.left == last);
+            last = curr;
+            curr = curr.right;
+        } while (last.right != head);
     }
 }
