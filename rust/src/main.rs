4f4c10f8352f7c37b0f88588c5dd24407ce39061
luo.fucong <luo.fucong@immomo.com>
2020-09-13 23:10:32 +0800

    https://leetcode-cn.com/problems/ba-shu-zu-pai-cheng-zui-xiao-de-shu-lcof/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,26 +3,29 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    pub fn min_number(nums: Vec<i32>) -> String {
         if nums.len() == 0 {
-            return 0;
+            return "".to_string();
         }
-        let n = nums.len();
-        // dp[i][0]: i nums, max sub array ends in nums[i]
-        // dp[i][1]: i nums, max sub array NOT ends in nums[i]
-        let mut dp = vec![vec![0; 2]; n];
-        dp[0][0] = nums[0];
-        dp[0][1] = std::i32::MIN;
-        for i in 1..n {
-            let num = nums[i];
-            dp[i][0] = std::cmp::max(dp[i - 1][0] + num, num);
-            dp[i][1] = std::cmp::max(dp[i - 1][0], dp[i - 1][1]);
+        let nums = &mut { nums };
+        nums.sort_unstable_by(|&a, &b| {
+            let a = a.to_string();
+            let b = b.to_string();
+            let mut ab = a.clone();
+            ab.push_str(&b);
+            let mut ba = b.clone();
+            ba.push_str(&a);
+            ab.cmp(&ba)
+        });
+        let mut s = String::new();
+        for num in nums {
+            s.push_str(&num.to_string());
         }
-        std::cmp::max(dp[n - 1][0], dp[n - 1][1])
+        s
     }
 }
 
 fn main() {
-    assert_eq!(6, Solution::max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-    assert_eq!(-1, Solution::max_sub_array(vec![-1]));
+    assert_eq!("102".to_string(), Solution::min_number(vec![2, 10]));
+    assert_eq!("3033459".to_string(), Solution::min_number(vec![3, 30, 34, 5, 9]));
 }
