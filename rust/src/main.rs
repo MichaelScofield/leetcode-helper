bea9c2a5f98f8661f0bd8e37711f21233d94269e
LFC <luo.fucong@immomo.com>
2020-09-14 12:03:31 +0800

    https://leetcode.com/problems/sliding-window-maximum/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,23 +3,50 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn first_uniq_char(s: String) -> i32 {
-        if s.len() == 0 {
-            return -1;
+    pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let len = nums.len();
+        assert!(len >= 1 && k >= 1 && k <= len as i32);
+        let k = k as usize;
+        use std::collections::VecDeque;
+        let monotonic_queue = &mut VecDeque::new();
+        fn enqueue(monotonic_queue: &mut VecDeque<i32>, v: i32) {
+            while let Some(&back) = monotonic_queue.back() {
+                if back < v {
+                    monotonic_queue.pop_back();
+                } else {
+                    break;
+                }
+            }
+            monotonic_queue.push_back(v);
         }
-        let chars = s.as_bytes();
-        let mut index = vec![(0, -1); 26];
-        for i in 0..chars.len() {
-            let offset = (chars[i] - 97) as usize;
-            index[offset].0 += 1;
-            index[offset].1 = i as i32;
+        fn dequeue(monotonic_queue: &mut VecDeque<i32>, expect: i32) {
+            if let Some(&v) = monotonic_queue.front() {
+                if v == expect {
+                    monotonic_queue.pop_front();
+                }
+            }
         }
-        index.iter().filter(|(count, _)| *count == 1).map(|(_, pos)| *pos).min().unwrap_or(-1)
+        fn max(monotonic_queue: &VecDeque<i32>) -> i32 {
+            *monotonic_queue.front().unwrap()
+        }
+        let mut sliding_window_max = Vec::with_capacity(len - k + 1);
+        for i in 0..len {
+            if i < k {
+                enqueue(monotonic_queue, nums[i]);
+            } else {
+                sliding_window_max.push(max(monotonic_queue));
+                enqueue(monotonic_queue, nums[i]);
+                dequeue(monotonic_queue, nums[i - k]);
+            }
+        }
+        sliding_window_max.push(max(monotonic_queue));
+        sliding_window_max
     }
 }
 
 fn main() {
-    assert_eq!(-1, Solution::first_uniq_char("cc".to_string()));
-    assert_eq!(0, Solution::first_uniq_char("leetcode".to_string()));
-    assert_eq!(2, Solution::first_uniq_char("loveleetcode".to_string()));
+    assert_eq!(vec![3, 3, 5, 5, 6, 7],
+               Solution::max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 3));
+    assert_eq!(vec![7],
+               Solution::max_sliding_window(vec![1, 3, -1, -3, 5, 3, 6, 7], 8));
 }
