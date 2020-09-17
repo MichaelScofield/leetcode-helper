d03a8412caaf4b2efb63421a1d35b07f458e250f
luo.fucong <luo.fucong@immomo.com>
2020-09-17 23:31:44 +0800

    https://leetcode.com/problems/kth-smallest-element-in-a-bst/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,63 +1,38 @@
 public class Solution {
 
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if (headA == null || headB == null) {
+    int k;
+
+    public int kthSmallest(TreeNode root, int k) {
+        assert k >= 1;
+        this.k = k;
+        return kthSmallest(root).val;
+    }
+
+    TreeNode kthSmallest(TreeNode root) {
+        if (root == null) {
             return null;
         }
-        int l1 = len(headA), l2 = len(headB);
-        if (l1 > l2) {
-            headA = forward(headA, l1 - l2);
-        } else {
-            headB = forward(headB, l2 - l1);
+        if (root.left == null && root.right == null) {
+            return --k == 0 ? root : null;
         }
-        while (headA != null && headB != null) {
-            if (headA == headB) {
-                return headA;
+        if (root.left != null) {
+            TreeNode node = kthSmallest(root.left);
+            if (node != null) {
+                return node;
             }
-            headA = headA.next;
-            headB = headB.next;
         }
-        return null;
-    }
-
-    int len(ListNode head) {
-        int l = 0;
-        while (head != null) {
-            l += 1;
-            head = head.next;
+        if (--k == 0) {
+            return root;
         }
-        return l;
-    }
-
-    ListNode forward(ListNode head, int n) {
-        while (n-- > 0) {
-            head = head.next;
-        }
-        return head;
+        return kthSmallest(root.right);
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-
-        ListNode n1_1 = new ListNode(4);
-        ListNode n1_2 = new ListNode(1);
-        ListNode n1_3 = new ListNode(8);
-        ListNode n1_4 = new ListNode(4);
-        ListNode n1_5 = new ListNode(5);
-        n1_1.next = n1_2;
-        n1_2.next = n1_3;
-        n1_3.next = n1_4;
-        n1_4.next = n1_5;
-        ListNode n2_1 = new ListNode(5);
-        ListNode n2_2 = new ListNode(6);
-        ListNode n2_3 = new ListNode(1);
-        n2_1.next = n2_2;
-        n2_2.next = n2_3;
-        n2_3.next = n1_3;
-        System.out.println(solution.getIntersectionNode(n1_1, n2_1) == n1_3);
-
-        ListNode headA = ListNode.from(new Integer[]{2, 6, 4}, null);
-        ListNode headB = ListNode.from(new Integer[]{1, 5}, null);
-        System.out.println(solution.getIntersectionNode(headA, headB) == null);
+        TreeNode root;
+        root = TreeNode.createTree(new Integer[]{3, 1, 4, null, 2});
+        System.out.println(solution.kthSmallest(root, 1)); // 1
+        root = TreeNode.createTree(new Integer[]{5, 3, 6, 2, 4, null, null, 1, null});
+        System.out.println(solution.kthSmallest(root, 3)); // 3
     }
 }
