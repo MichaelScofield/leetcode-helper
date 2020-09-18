bbcf2abf58ef28393db75949480e3f9dfb968522
luo.fucong <luo.fucong@immomo.com>
2020-09-18 23:24:09 +0800

    https://leetcode.com/problems/single-number-ii/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,45 +3,30 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn single_number(nums: Vec<i32>) -> Vec<i32> {
-        assert!(nums.len() >= 2);
-        let nums = &mut { nums };
-
-        fn xor(nums: &Vec<i32>, i: usize, j: usize) -> i32 {
-            let mut xor = nums[i];
-            for k in i + 1..j {
-                xor ^= nums[k];
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        assert!(nums.len() > 0);
+        let mut bits_count = vec![0; 32];
+        for num in nums {
+            for i in 0..32 {
+                if num & (1 << i) != 0 {
+                    bits_count[i] += 1;
+                }
             }
-            xor
         }
 
-        let x = xor(nums, 0, nums.len());
-        let mut bit_mask = 1;
+        let mut n = 0;
         for i in 0..32 {
-            bit_mask = 1 << i;
-            if x & bit_mask == bit_mask {
-                break;
+            let p = bits_count[i] % 3;
+            if p == 1 {
+                n += 2i32.pow(i as u32);
             }
         }
-        assert_ne!(bit_mask, 0);
-
-        let mut i = 0;
-        let mut j = nums.len() - 1;
-        while i <= j {
-            if &nums[i] & bit_mask == bit_mask {
-                nums.swap(i, j);
-                j -= 1;
-            } else {
-                i += 1;
-            }
-        }
-        let p = j + 1;
-        vec![xor(nums, 0, p), xor(nums, p, nums.len())]
+        n
     }
 }
 
 fn main() {
-    assert_eq!(vec![5, 3], Solution::single_number(vec![1, 2, 1, 3, 2, 5]));
-    assert_eq!(vec![0, -1], Solution::single_number(vec![-1, 0]));
-    assert_eq!(vec![0, 1], Solution::single_number(vec![0, 1]));
+    assert_eq!(3, Solution::single_number(vec![2, 2, 3, 2]));
+    assert_eq!(99, Solution::single_number(vec![0, 1, 0, 1, 0, 1, 99]));
+    assert_eq!(1, Solution::single_number(vec![1]));
 }
