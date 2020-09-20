c6cf22dece0f280db7d34ba8536a29ddd4f43276
LFC <luo.fucong@immomo.com>
2020-09-20 11:35:24 +0800

    https://leetcode-cn.com/contest/weekly-contest-207/problems/maximum-non-negative-product-in-a-matrix/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,28 +3,34 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn max_unique_split(s: String) -> i32 {
-        let len = s.len();
-        assert!(len > 0);
-        let s = s.as_str();
-        use std::collections::HashSet;
-        let mut substrs = HashSet::new();
-        let mut i = 0;
-        let mut j = i + 1;
-        while j <= len {
-            if !substrs.contains(&s[i..j]) {
-                substrs.insert(&s[i..j]);
-                i = j;
-            }
-            j += 1;
+    pub fn max_product_path(grid: Vec<Vec<i32>>) -> i32 {
+        assert!(grid.len() > 0 && grid[0].len() > 0);
+        let rows = grid.len();
+        let cols = grid[0].len();
+        let mut dp = vec![vec![0; cols + 1]; rows + 1];
+        for i in 0..rows {
+            dp[i][0] = grid[i][0] as i64;
         }
-        substrs.len() as i32
+        for i in 0..cols {
+            dp[0][i] = grid[0][i] as i64;
+        }
+        for i in 1..=rows {
+            for j in 1..=cols {
+                let num = grid[i - 1][j - 1] as i64;
+                dp[i][j] = std::cmp::max(dp[i - 1][j] * num, dp[i][j - 1] * num);
+            }
+        }
+        if dp[rows][cols] < 0 {
+            -1
+        } else {
+            (dp[rows][cols] % 1_000_000_007) as i32
+        }
     }
 }
 
 fn main() {
-    assert_eq!(5, Solution::max_unique_split("addbsd".to_string()));
-    assert_eq!(5, Solution::max_unique_split("ababccc".to_string()));
-    assert_eq!(2, Solution::max_unique_split("aba".to_string()));
-    assert_eq!(1, Solution::max_unique_split("aa".to_string()));
+    assert_eq!(-1, Solution::max_product_path(vecvec![[-1,-2,-3],[-2,-3,-3],[-3,-3,-2]]));
+    assert_eq!(8, Solution::max_product_path(vecvec![[1,-2,1],[1,-2,1],[3,-4,1]]));
+    assert_eq!(0, Solution::max_product_path(vecvec![[1, 3],[0,-4]]));
+    assert_eq!(2, Solution::max_product_path(vecvec![[ 1, 4,4,0],[-2, 0,0,1],[ 1,-1,1,1]]));
 }
