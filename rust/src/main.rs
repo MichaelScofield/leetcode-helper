a925ba562e987daffa0b8a2c0d29d5d7afd48228
luo.fucong <luo.fucong@immomo.com>
2020-09-22 23:04:31 +0800

    https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,45 +3,19 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn reverse_words(s: String) -> String {
-        let mut chars = s.trim().chars().collect::<Vec<char>>();
+    pub fn reverse_left_words(s: String, n: i32) -> String {
+        assert!(n >= 1 && s.len() > n as usize);
+        let mut chars = s.chars().collect::<Vec<char>>();
         let len = chars.len();
-        if len == 0 {
-            return "".to_string();
-        }
-        chars.reverse();
-        let mut reverse: Vec<char> = Vec::with_capacity(len);
-        let mut i = 0;
-        let mut j = 1;
-        while j < len {
-            if chars[i] == ' ' {
-                if chars[j] != ' ' {
-                    i = j;
-                }
-            } else {
-                if chars[j] == ' ' {
-                    let word = &mut chars[i..j].to_vec();
-                    word.reverse();
-                    reverse.append(word);
-                    reverse.push(' ');
-                    i = j;
-                }
-            }
-            j += 1;
-        }
-        if chars[i] != ' ' {
-            let word = &mut chars[i..len].to_vec();
-            word.reverse();
-            reverse.append(word);
-        }
-        reverse.iter().collect()
+        let n = n as usize;
+        chars[0..len].reverse();
+        chars[0..len - n].reverse();
+        chars[len - n..len].reverse();
+        chars.iter().collect()
     }
 }
 
 fn main() {
-    assert_eq!("blue is sky the".to_string(), Solution::reverse_words("the sky is blue".to_string()));
-    assert_eq!("world! hello".to_string(), Solution::reverse_words("  hello world!  ".to_string()));
-    assert_eq!("example good a".to_string(), Solution::reverse_words("a good   example".to_string()));
-    assert_eq!("example".to_string(), Solution::reverse_words("example".to_string()));
-    assert_eq!("".to_string(), Solution::reverse_words("  ".to_string()));
+    assert_eq!("cdefgab".to_string(), Solution::reverse_left_words("abcdefg".to_string(), 2));
+    assert_eq!("umghlrlose".to_string(), Solution::reverse_left_words("lrloseumgh".to_string(), 6));
 }
