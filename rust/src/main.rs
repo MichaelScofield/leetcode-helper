076fa6325b962ed68a81280ea3b8d16d18c70610
luo.fucong <luo.fucong@immomo.com>
2020-09-22 22:54:44 +0800

    https://leetcode-cn.com/problems/reverse-words-in-a-string/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,30 +3,45 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn find_continuous_sequence(target: i32) -> Vec<Vec<i32>> {
-        assert!(target > 0);
-        let mut i = 1;
-        let mut j = 2;
-        let mut sequence = vec![];
-        loop {
-            let sum = (i + j) * (j - i + 1) / 2;
-            if sum < target {
-                j += 1;
-            } else {
-                if sum == target {
-                    sequence.push((i..=j).collect());
-                }
-                i += 1;
-            }
-            if i * 2 >= target {
-                break;
-            }
+    pub fn reverse_words(s: String) -> String {
+        let mut chars = s.trim().chars().collect::<Vec<char>>();
+        let len = chars.len();
+        if len == 0 {
+            return "".to_string();
         }
-        sequence
+        chars.reverse();
+        let mut reverse: Vec<char> = Vec::with_capacity(len);
+        let mut i = 0;
+        let mut j = 1;
+        while j < len {
+            if chars[i] == ' ' {
+                if chars[j] != ' ' {
+                    i = j;
+                }
+            } else {
+                if chars[j] == ' ' {
+                    let word = &mut chars[i..j].to_vec();
+                    word.reverse();
+                    reverse.append(word);
+                    reverse.push(' ');
+                    i = j;
+                }
+            }
+            j += 1;
+        }
+        if chars[i] != ' ' {
+            let word = &mut chars[i..len].to_vec();
+            word.reverse();
+            reverse.append(word);
+        }
+        reverse.iter().collect()
     }
 }
 
 fn main() {
-    assert_eq!(vecvec![[2,3,4],[4,5]], Solution::find_continuous_sequence(9));
-    assert_eq!(vecvec![[1,2,3,4,5],[4,5,6],[7,8]], Solution::find_continuous_sequence(15));
+    assert_eq!("blue is sky the".to_string(), Solution::reverse_words("the sky is blue".to_string()));
+    assert_eq!("world! hello".to_string(), Solution::reverse_words("  hello world!  ".to_string()));
+    assert_eq!("example good a".to_string(), Solution::reverse_words("a good   example".to_string()));
+    assert_eq!("example".to_string(), Solution::reverse_words("example".to_string()));
+    assert_eq!("".to_string(), Solution::reverse_words("  ".to_string()));
 }
