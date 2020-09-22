10b1d2a26d8ac2e83fd566c1692c15fc8a1b75f3
luo.fucong <luo.fucong@immomo.com>
2020-09-22 22:21:50 +0800

    https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,41 +3,30 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn pancake_sort(arr: Vec<i32>) -> Vec<i32> {
-        assert!(arr.len() > 0);
-        fn flip(arr: &mut [i32], n: usize) {
-            let mut i = 0;
-            let mut j = n;
-            while i < j {
-                arr.swap(i, j);
-                i += 1;
-                j -= 1;
-            }
-        }
-        fn pancake_sort(arr: &mut [i32], flips: &mut Vec<i32>) {
-            let len = arr.len();
-            if len == 0 {
-                return;
-            }
-            let max_index = arr.iter().enumerate().max_by_key(|e| e.1).unwrap().0;
-            if max_index < len - 1 {
-                if max_index > 0 {
-                    flip(arr, max_index);
-                    flips.push(max_index as i32 + 1);
+    pub fn find_continuous_sequence(target: i32) -> Vec<Vec<i32>> {
+        assert!(target > 0);
+        let mut i = 1;
+        let mut j = 2;
+        let mut sequence = vec![];
+        loop {
+            let sum = (i + j) * (j - i + 1) / 2;
+            if sum < target {
+                j += 1;
+            } else {
+                if sum == target {
+                    sequence.push((i..=j).collect());
                 }
-                flip(arr, len - 1);
-                flips.push(len as i32);
+                i += 1;
             }
-            pancake_sort(&mut arr[..len - 1], flips)
+            if i * 2 >= target {
+                break;
+            }
         }
-        let arr = &mut { arr };
-        let mut flips = Vec::with_capacity(arr.len());
-        pancake_sort(arr.as_mut_slice(), &mut flips);
-        flips
+        sequence
     }
 }
 
 fn main() {
-    assert_eq!(vec![3, 4, 2, 3, 2], Solution::pancake_sort(vec![3, 2, 4, 1]));
-    assert_eq!(Vec::<i32>::new(), Solution::pancake_sort(vec![1, 2, 3]));
+    assert_eq!(vecvec![[2,3,4],[4,5]], Solution::find_continuous_sequence(9));
+    assert_eq!(vecvec![[1,2,3,4,5],[4,5,6],[7,8]], Solution::find_continuous_sequence(15));
 }
