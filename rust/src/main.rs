7f47b181c5f53906ebe69f069d0446cf23836229
luo.fucong <luo.fucong@immomo.com>
2020-09-25 22:47:34 +0800

    https://leetcode-cn.com/problems/interval-list-intersections/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,34 +3,35 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn remove_covered_intervals(intervals: Vec<Vec<i32>>) -> i32 {
-        assert!(intervals.len() > 0);
-        if intervals.len() == 1 {
-            return 1;
+    pub fn interval_intersection(a: Vec<Vec<i32>>, b: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        if a.len() == 0 || b.len() == 0 {
+            return vec![];
         }
-        let intervals = &mut { intervals };
-        use std::cmp::Ordering;
-        intervals.sort_by(|a, b| {
-            let ordering = a[0].cmp(&b[0]);
-            if ordering == Ordering::Equal {
-                b[1].cmp(&a[1])
+        let mut intersections = vec![];
+        let mut i = 0;
+        let mut j = 0;
+        while i < a.len() && j < b.len() {
+            let a = &a[i];
+            let b = &b[j];
+            if b[0] > a[1] {
+                i += 1;
+            } else if a[0] > b[1] {
+                j += 1;
             } else {
-                ordering
-            }
-        });
-        let mut max = &intervals[0];
-        let mut remain = 1;
-        for i in 1..intervals.len() {
-            let curr = &intervals[i];
-            if curr[1] > max[1] {
-                remain += 1;
-                max = curr;
+                intersections.push(vec![std::cmp::max(a[0], b[0]), std::cmp::min(a[1], b[1])]);
+                if a[1] < b[1] {
+                    i += 1;
+                } else {
+                    j += 1;
+                }
             }
         }
-        remain
+        intersections
     }
 }
 
 fn main() {
-    assert_eq!(2, Solution::remove_covered_intervals(vecvec![[1,4],[3,6],[2,8]]));
+    let a = vecvec![[0,2],[5,10],[13,23],[24,25]];
+    let b = vecvec![[1,5],[8,12],[15,24],[25,26]];
+    assert_eq!(vecvec![[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]], Solution::interval_intersection(a, b));
 }
