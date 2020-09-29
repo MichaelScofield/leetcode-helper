3d72c322971461799e5b2df8d55312d5d7da1e45
LFC <luo.fucong@immomo.com>
2020-09-29 10:54:59 +0800

    https://leetcode-cn.com/problems/shuffle-an-array/

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,28 +1,36 @@
-public class Solution {
+import java.util.Arrays;
+import java.util.Random;
 
-    public void flatten(TreeNode root) {
-        if (root == null) {
-            return;
+class Solution {
+
+    final int[] nums;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int[] reset() {
+        return nums;
+    }
+
+    public int[] shuffle() {
+        int[] copy = Arrays.copyOf(nums, nums.length);
+        Random random = new Random(System.nanoTime());
+        for (int i = 0; i < copy.length; i++) {
+            int r = random.nextInt(copy.length);
+            int tmp = copy[i];
+            copy[i] = copy[r];
+            copy[r] = tmp;
         }
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        flatten(left);
-        flatten(right);
-        if (left != null) {
-            TreeNode rightMost = left;
-            while (rightMost.right != null) {
-                rightMost = rightMost.right;
-            }
-            rightMost.right = right;
-            root.right = left;
-            root.left = null;
-        }
+        return copy;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        TreeNode root = TreeNode.createTree(new Integer[]{1, 2, 5, 3, 4, null, 6});
-        solution.flatten(root);
-        TreeNode.printTree(root);
+        Solution solution = new Solution(new int[]{1, 2, 3});
+        System.out.println(Arrays.toString(solution.shuffle()));
+        System.out.println(Arrays.toString(solution.reset()));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(solution.shuffle()));
+        }
     }
 }
