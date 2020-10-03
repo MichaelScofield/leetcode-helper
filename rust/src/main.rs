62c2c7386ba86e10acdb4d5f29de5bf6d0649174
luo.fucong <luo.fucong@immomo.com>
2020-10-03 23:17:03 +0800

    https://leetcode-cn.com/problems/trapping-rain-water/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,49 +3,47 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn ship_within_days(weights: Vec<i32>, d: i32) -> i32 {
-        let len = weights.len();
-        assert!(d >= 1);
-        assert!(len >= d as usize);
-
-        fn ship_days(weights: &Vec<i32>, capacity: i32) -> i32 {
-            let mut days = 0;
-            let mut sum = 0;
-            let mut i = 0;
-            while i < weights.len() {
-                sum += weights[i];
-                if sum > capacity {
-                    days += 1;
-                    sum = weights[i];
+    pub fn trap(height: Vec<i32>) -> i32 {
+        let len = height.len();
+        if len < 1 {
+            return 0;
+        }
+        let mut rain = 0;
+        let mut i = 0;
+        let mut j = len - 1;
+        let mut last_sink_height = 0;
+        while i < j {
+            while i < j {
+                if height[i] > last_sink_height {
+                    break;
                 }
                 i += 1;
             }
-            days + 1
-        }
-
-        let mut min_capacity = std::i32::MIN;
-        let mut max_capacity = 0;
-        for &w in weights.iter() {
-            min_capacity = std::cmp::max(min_capacity, w);
-            max_capacity += w;
-        }
-
-        let mut i = min_capacity;
-        let mut j = max_capacity;
-        while i <= j {
-            let capacity = i + (j - i) / 2;
-            if ship_days(&weights, capacity) <= d {
-                j = capacity - 1;
-            } else {
-                i = capacity + 1;
+            while i < j {
+                if height[j] > last_sink_height {
+                    break;
+                }
+                j -= 1;
+            }
+            if i < j {
+                let sink_height = std::cmp::min(height[i], height[j]);
+                for k in i + 1..j {
+                    if height[k] <= last_sink_height {
+                        rain += sink_height - last_sink_height;
+                    } else if height[k] < sink_height {
+                        rain += sink_height - height[k];
+                    }
+                }
+                last_sink_height = sink_height;
             }
         }
-        j + 1
+        rain
     }
 }
 
 fn main() {
-    assert_eq!(15, Solution::ship_within_days(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
-    assert_eq!(6, Solution::ship_within_days(vec![3, 2, 2, 4, 1, 4], 3));
-    assert_eq!(3, Solution::ship_within_days(vec![1, 2, 3, 1, 1], 4));
+    assert_eq!(6, Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+    assert_eq!(1, Solution::trap(vec![100, 0, 1]));
+    assert_eq!(0, Solution::trap(vec![100, 1]));
+    assert_eq!(2, Solution::trap(vec![2, 0, 2]));
 }
