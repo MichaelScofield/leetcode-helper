295286f37977bd39e3e454027268afb252f8b12b
luo.fucong <luo.fucong@immomo.com>
2020-10-07 22:24:51 +0800

    https://leetcode-cn.com/problems/house-robber/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,22 +3,21 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn jump(nums: Vec<i32>) -> i32 {
+    pub fn rob(nums: Vec<i32>) -> i32 {
         let len = nums.len();
-        assert!(len > 0);
-        let mut dp = vec![std::i32::MAX; len];
-        dp[0] = 0;
-        for i in 1..len {
-            for j in 0..i {
-                if nums[j] >= (i - j) as i32 {
-                    dp[i] = std::cmp::min(dp[i], dp[j] + 1);
-                }
-            }
+        if len == 0 {
+            return 0;
         }
-        dp[len - 1]
+        let mut dp = vec![0; len + 1];
+        dp[1] = nums[0];
+        for i in 2..=len {
+            dp[i] = std::cmp::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+        }
+        dp[len]
     }
 }
 
 fn main() {
-    assert_eq!(2, Solution::jump(vec![2, 3, 1, 1, 4]));
+    assert_eq!(4, Solution::rob(vec![1, 2, 3, 1]));
+    assert_eq!(12, Solution::rob(vec![2, 7, 9, 3, 1]));
 }
