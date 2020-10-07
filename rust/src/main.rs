3639c3cc6418e44c0f726acd5ec907978c68eb45
luo.fucong <luo.fucong@immomo.com>
2020-10-07 23:49:58 +0800

    https://leetcode-cn.com/problems/house-robber-iii/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,33 +1,42 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use helper::def::TreeNode;
+
 mod helper;
 
 struct Solution;
 
 impl Solution {
-    pub fn rob(nums: Vec<i32>) -> i32 {
-        let len = nums.len();
-        if len == 0 {
-            return 0;
-        }
-        if len == 1 {
-            return nums[0];
-        }
-        fn rob(nums: &[i32]) -> i32 {
-            let len = nums.len();
-            let mut dp = vec![0; len + 1];
-            dp[1] = nums[0];
-            for i in 2..=len {
-                dp[i] = std::cmp::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+    pub fn rob(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(root) = root {
+            let root = root.borrow();
+
+            let mut rob_root = root.val;
+            if let Some(left) = &root.left {
+                let left = left.borrow();
+                rob_root += Solution::rob(left.left.clone()) + Solution::rob(left.right.clone());
             }
-            dp[len]
+            if let Some(right) = &root.right {
+                let right = right.borrow();
+                rob_root += Solution::rob(right.left.clone()) + Solution::rob(right.right.clone());
+            }
+
+            let rob_left = Solution::rob(root.left.clone());
+            let rob_right = Solution::rob(root.right.clone());
+
+            std::cmp::max(rob_root, rob_left + rob_right)
+        } else {
+            0
         }
-        std::cmp::max(rob(&nums[0..len - 1]), rob(&nums[1..len]))
     }
 }
 
 fn main() {
-    assert_eq!(2, Solution::rob(vec![2]));
-    assert_eq!(2, Solution::rob(vec![1, 2]));
-    assert_eq!(100, Solution::rob(vec![1, 2, 100]));
-    assert_eq!(3, Solution::rob(vec![2, 3, 2]));
-    assert_eq!(4, Solution::rob(vec![1, 2, 3, 1]));
+    let root = helper::util::create_binary_tree(vec![2, 1, 3, -1, 4]);
+    assert_eq!(7, Solution::rob(root));
+    let root = helper::util::create_binary_tree(vec![3, 2, 3, -1, 3, -1, 1]);
+    assert_eq!(7, Solution::rob(root));
+    let root = helper::util::create_binary_tree(vec![3, 4, 5, 1, 3, -1, 1]);
+    assert_eq!(9, Solution::rob(root));
 }
