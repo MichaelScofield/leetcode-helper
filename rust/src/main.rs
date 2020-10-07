3a25a9bc968d32de030618022188d8015a9a0a2c
luo.fucong <luo.fucong@immomo.com>
2020-10-07 23:05:41 +0800

    https://leetcode-cn.com/problems/house-robber-ii/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,16 +8,26 @@ impl Solution {
         if len == 0 {
             return 0;
         }
-        let mut dp = vec![0; len + 1];
-        dp[1] = nums[0];
-        for i in 2..=len {
-            dp[i] = std::cmp::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+        if len == 1 {
+            return nums[0];
         }
-        dp[len]
+        fn rob(nums: &[i32]) -> i32 {
+            let len = nums.len();
+            let mut dp = vec![0; len + 1];
+            dp[1] = nums[0];
+            for i in 2..=len {
+                dp[i] = std::cmp::max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+            }
+            dp[len]
+        }
+        std::cmp::max(rob(&nums[0..len - 1]), rob(&nums[1..len]))
     }
 }
 
 fn main() {
+    assert_eq!(2, Solution::rob(vec![2]));
+    assert_eq!(2, Solution::rob(vec![1, 2]));
+    assert_eq!(100, Solution::rob(vec![1, 2, 100]));
+    assert_eq!(3, Solution::rob(vec![2, 3, 2]));
     assert_eq!(4, Solution::rob(vec![1, 2, 3, 1]));
-    assert_eq!(12, Solution::rob(vec![2, 7, 9, 3, 1]));
 }
