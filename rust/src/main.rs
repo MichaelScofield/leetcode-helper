4df13458ea8069e13b09dee4600399ecae86e73e
luo.fucong <luo.fucong@immomo.com>
2020-10-08 15:09:17 +0800

    https://leetcode-cn.com/problems/corporate-flight-bookings/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,42 +1,23 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use helper::def::TreeNode;
-
 mod helper;
 
 struct Solution;
 
 impl Solution {
-    pub fn rob(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if let Some(root) = root {
-            let root = root.borrow();
-
-            let mut rob_root = root.val;
-            if let Some(left) = &root.left {
-                let left = left.borrow();
-                rob_root += Solution::rob(left.left.clone()) + Solution::rob(left.right.clone());
+    pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
+        let mut diff = vec![0; n as usize];
+        for booking in bookings.iter() {
+            diff[booking[0] as usize - 1] += booking[2];
+            if booking[1] < n {
+                diff[booking[1] as usize] -= booking[2];
             }
-            if let Some(right) = &root.right {
-                let right = right.borrow();
-                rob_root += Solution::rob(right.left.clone()) + Solution::rob(right.right.clone());
-            }
-
-            let rob_left = Solution::rob(root.left.clone());
-            let rob_right = Solution::rob(root.right.clone());
-
-            std::cmp::max(rob_root, rob_left + rob_right)
-        } else {
-            0
         }
+        for i in 1..diff.len() {
+            diff[i] += diff[i - 1];
+        }
+        diff
     }
 }
 
 fn main() {
-    let root = helper::util::create_binary_tree(vec![2, 1, 3, -1, 4]);
-    assert_eq!(7, Solution::rob(root));
-    let root = helper::util::create_binary_tree(vec![3, 2, 3, -1, 3, -1, 1]);
-    assert_eq!(7, Solution::rob(root));
-    let root = helper::util::create_binary_tree(vec![3, 4, 5, 1, 3, -1, 1]);
-    assert_eq!(9, Solution::rob(root));
+    assert_eq!(vec![10, 55, 45, 25, 25], Solution::corp_flight_bookings(vecvec![[1,2,10],[2,3,20],[2,5,25]], 5));
 }
