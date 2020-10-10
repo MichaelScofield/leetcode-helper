ba3c5ee83c32e244776e1c3ec2de90a25b1ef8b6
LFC <luo.fucong@immomo.com>
2020-10-10 11:58:00 +0800

    https://leetcode-cn.com/problems/burst-balloons/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,35 +3,24 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn longest_palindrome_subseq(s: String) -> i32 {
-        let bytes = s.as_bytes();
-        let len = bytes.len();
-        let mut dp = vec![vec![0; len]; len];
-        for i in 0..len {
-            dp[i][i] = 1;
-        }
-        for j in 1..len {
+    pub fn max_coins(nums: Vec<i32>) -> i32 {
+        let len = nums.len();
+        let mut dp = vec![vec![0; len + 2]; len + 2];
+        for j in 1..=(len + 1) {
             for i in (0..j).rev() {
-                let mut equal_index = None;
-                for x in i..j {
-                    if bytes[x] == bytes[j] {
-                        equal_index = Some(x);
-                        break;
-                    }
-                }
-                dp[i][j] = if let Some(x) = equal_index {
-                    std::cmp::max(dp[x + 1][j - 1] + 2, dp[i][j - 1])
-                } else {
-                    dp[i][j - 1]
+                for k in (i + 1)..j {
+                    let curr = nums[k - 1];
+                    let left = if i == 0 { 1 } else { nums[i - 1] };
+                    let right = if j == len + 1 { 1 } else { nums[j - 1] };
+                    dp[i][j] = std::cmp::max(dp[i][j],
+                                             dp[i][k] + dp[k][j] + left * curr * right);
                 }
             }
         }
-        dp[0][len - 1]
+        dp[0][len + 1]
     }
 }
 
 fn main() {
-    assert_eq!(6, Solution::longest_palindrome_subseq("abaabaa".to_string()));
-    assert_eq!(4, Solution::longest_palindrome_subseq("bbbab".to_string()));
-    assert_eq!(2, Solution::longest_palindrome_subseq("cbbd".to_string()));
+    assert_eq!(167, Solution::max_coins(vec![3, 1, 5, 8]));
 }
