7120343f0089fbaa767dae377060124c29812e6c
LFC <luo.fucong@immomo.com>
2020-10-28 11:55:24 +0800

    https://leetcode-cn.com/problems/minimum-ascii-delete-sum-for-two-strings/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,30 +3,34 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn min_distance(word1: String, word2: String) -> i32 {
-        fn longest_common_subsequence(text1: &[u8], text2: &[u8]) -> usize {
-            let m = text1.len();
-            let n = text2.len();
-            let mut dp = vec![vec![0; n + 1]; m + 1];
-            for i in 1..=m {
-                for j in 1..=n {
-                    dp[i][j] = std::cmp::max(
-                        dp[i - 1][j - 1] + if text1[i - 1] == text2[j - 1] { 1 } else { 0 },
-                        std::cmp::max(dp[i][j - 1], dp[i - 1][j]));
+    pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
+        let s1 = s1.as_bytes();
+        let s2 = s2.as_bytes();
+        let m = s1.len();
+        let n = s2.len();
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+        for i in 1..=m {
+            dp[i][0] = dp[i - 1][0] + s1[i - 1] as i32;
+        }
+        for j in 1..=n {
+            dp[0][j] = dp[0][j - 1] + s2[j - 1] as i32;
+        }
+        for i in 1..=m {
+            for j in 1..=n {
+                if s1[i - 1] == s2[j - 1] {
+                    dp[i][j] = dp[i - 1][j - 1]
+                } else {
+                    dp[i][j] = std::cmp::min(
+                        dp[i][j - 1] + s2[j - 1] as i32,
+                        dp[i - 1][j] + s1[i - 1] as i32)
                 }
             }
-            dp[m][n]
         }
-        let text1 = word1.as_bytes();
-        let text2 = word2.as_bytes();
-        let lcs = longest_common_subsequence(text1, text2);
-        (text1.len() - lcs + text2.len() - lcs) as i32
+        dp[m][n]
     }
 }
 
 fn main() {
-    assert_eq!(2, Solution::min_distance("sea".to_string(), "eat".to_string()));
-    assert_eq!(3, Solution::min_distance("sea".to_string(), "".to_string()));
-    assert_eq!(0, Solution::min_distance("sea".to_string(), "sea".to_string()));
-    assert_eq!(4, Solution::min_distance("sea".to_string(), "b".to_string()));
+    assert_eq!(231, Solution::minimum_delete_sum("sea".to_string(), "eat".to_string()));
+    assert_eq!(403, Solution::minimum_delete_sum("delete".to_string(), "leet".to_string()));
 }
