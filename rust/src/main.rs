54d40f76ce3940a99c7e6b90a1f71fe8fcde9146
luo.fucong <luo.fucong@immomo.com>
2020-11-08 10:47:00 +0800

    https://leetcode-cn.com/contest/weekly-contest-214/problems/get-maximum-in-generated-array/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,34 +3,29 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn minimum_delete_sum(s1: String, s2: String) -> i32 {
-        let s1 = s1.as_bytes();
-        let s2 = s2.as_bytes();
-        let m = s1.len();
-        let n = s2.len();
-        let mut dp = vec![vec![0; n + 1]; m + 1];
-        for i in 1..=m {
-            dp[i][0] = dp[i - 1][0] + s1[i - 1] as i32;
+    pub fn get_maximum_generated(n: i32) -> i32 {
+        assert!(n >= 0 && n <= 100);
+        if n == 0 || n == 1 {
+            return n;
         }
-        for j in 1..=n {
-            dp[0][j] = dp[0][j - 1] + s2[j - 1] as i32;
-        }
-        for i in 1..=m {
-            for j in 1..=n {
-                if s1[i - 1] == s2[j - 1] {
-                    dp[i][j] = dp[i - 1][j - 1]
-                } else {
-                    dp[i][j] = std::cmp::min(
-                        dp[i][j - 1] + s2[j - 1] as i32,
-                        dp[i - 1][j] + s1[i - 1] as i32)
-                }
+        let n = n as usize;
+        let mut nums = vec![0; n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for i in 2..=n {
+            if i % 2 == 0 {
+                nums[i] = nums[i / 2];
+            } else {
+                nums[i] = nums[(i - 1) / 2] + nums[(i - 1) / 2 + 1];
             }
         }
-        dp[m][n]
+        nums.sort();
+        nums[nums.len() - 1]
     }
 }
 
 fn main() {
-    assert_eq!(231, Solution::minimum_delete_sum("sea".to_string(), "eat".to_string()));
-    assert_eq!(403, Solution::minimum_delete_sum("delete".to_string(), "leet".to_string()));
+    assert_eq!(3, Solution::get_maximum_generated(7));
+    assert_eq!(1, Solution::get_maximum_generated(2));
+    assert_eq!(2, Solution::get_maximum_generated(3));
 }
