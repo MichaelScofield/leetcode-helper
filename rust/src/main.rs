8f546a8dceca9feb9692af5b7542d8e686dd29ec
luo.fucong <luo.fucong@immomo.com>
2021-11-20 16:29:06 +0800

    https://leetcode-cn.com/problems/combination-sum-ii/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    pub fn combination_sum2(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         assert!(candidates.len() >= 1);
         let candidates = &mut { candidates };
         candidates.sort();
@@ -15,7 +15,8 @@ impl Solution {
             results: &mut Vec<Vec<i32>>,
             start: usize,
         ) {
-            for i in start..candidates.len() {
+            let mut i = start;
+            while i < candidates.len() {
                 let candidate = candidates[i];
                 if target < candidate {
                     return;
@@ -24,9 +25,15 @@ impl Solution {
                 if target == candidate {
                     results.push(result.clone());
                 } else {
-                    combination_sum(&candidates, target - candidate, result, results, i);
+                    combination_sum(&candidates, target - candidate, result, results, i + 1);
                 }
-                result.pop();
+                let last = result.pop().unwrap();
+                while i < candidates.len() {
+                    if candidates[i] != last {
+                        break;
+                    }
+                    i += 1;
+                }
             }
         }
 
@@ -39,17 +46,28 @@ impl Solution {
 
 fn main() {
     assert_eq!(
-        vecvec![[2, 2, 3], [7]],
-        Solution::combination_sum(vec![2, 3, 6, 7], 7)
+        vecvec![[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]],
+        Solution::combination_sum2(vec![10, 1, 2, 7, 6, 1, 5], 8)
     );
     assert_eq!(
-        vecvec![[2, 2, 2, 2], [2, 3, 3], [3, 5]],
-        Solution::combination_sum(vec![2, 3, 5], 8)
+        vecvec![[1, 2, 2], [5]],
+        Solution::combination_sum2(vec![2, 5, 2, 1, 2], 5)
+    );
+    assert_eq!(
+        vecvec![[7]],
+        Solution::combination_sum2(vec![2, 3, 6, 7], 7)
+    );
+    assert_eq!(
+        vecvec![[3, 5]],
+        Solution::combination_sum2(vec![2, 3, 5], 8)
     );
     assert_eq!(
         Vec::<Vec<i32>>::new(),
-        Solution::combination_sum(vec![2], 1)
+        Solution::combination_sum2(vec![2], 1)
     );
-    assert_eq!(vecvec![[1]], Solution::combination_sum(vec![1], 1));
-    assert_eq!(vecvec![[1, 1]], Solution::combination_sum(vec![1], 2));
+    assert_eq!(vecvec![[1]], Solution::combination_sum2(vec![1], 1));
+    assert_eq!(
+        Vec::<Vec<i32>>::new(),
+        Solution::combination_sum2(vec![1], 2)
+    );
 }
