6b1453efd021b2de5e264d74852d606f6eda39e7
luo.fucong <luo.fucong@immomo.com>
2021-12-04 17:03:43 +0800

    https://leetcode-cn.com/problems/3sum-closest/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,92 +3,55 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
-        if s1.len() == 0 {
-            return s2 == s3;
-        }
-        if s2.len() == 0 {
-            return s1 == s3;
-        }
-        if s3.len() == 0 {
-            return s1 == "" && s2 == "";
-        }
-        if s1.len() + s2.len() != s3.len() {
-            return false;
-        }
+    pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
+        assert!(nums.len() >= 3);
 
-        fn f(s: &str) -> Vec<Vec<String>> {
-            if s.len() == 1 {
-                return vec![vec![s.to_string()]];
+        let nums = &mut { nums };
+        nums.sort();
+
+        let mut closest: i64 = i32::MAX as i64;
+        let target = target as i64;
+        let mut i = 0;
+        while i < nums.len() {
+            if i > 0 && nums[i] == nums[i - 1] {
+                i += 1;
+                continue;
             }
 
-            let mut partitions = vec![];
-            let remaining_partitions = f(&s[1..]);
-
-            for p in remaining_partitions.iter() {
-                let mut v = vec![];
-                v.push(String::from(&s[0..1]));
-                v.append(&mut p.iter().map(String::from).collect());
-                partitions.push(v);
-            }
-
-            for p in remaining_partitions.iter() {
-                let mut v = vec![];
-
-                let mut first = String::new();
-                first.push_str(&s[0..1]);
-                first.push_str(p[0].as_str());
-                v.push(first);
-
-                v.append(&mut p[1..].iter().map(String::from).collect());
-                partitions.push(v);
-            }
-            partitions
-        }
-        let partitions = f(s3.as_str());
-
-        for partition in partitions {
-            let mut a = String::new();
-            let mut b = String::new();
-            for i in 0..partition.len() {
-                if i % 2 == 0 {
-                    a.push_str(&partition[i]);
-                } else {
-                    b.push_str(&partition[i]);
+            let mut j = i + 1;
+            while j < nums.len() {
+                if j > i + 1 && nums[j] == nums[j - 1] {
+                    j += 1;
+                    continue;
                 }
+
+                let mut k = j + 1;
+                while k < nums.len() {
+                    if k > j + 1 && nums[k] == nums[k - 1] {
+                        k += 1;
+                        continue;
+                    }
+
+                    let sum = nums[i] as i64 + nums[j] as i64 + nums[k] as i64;
+                    let d = (sum - target).abs();
+                    if d == 0 {
+                        return sum as i32;
+                    }
+                    if d < (closest - target).abs() {
+                        closest = sum;
+                    }
+                    k += 1;
+                }
+                j += 1;
             }
-            if a == s1 && b == s2 || a == s2 && b == s1 {
-                return true;
-            }
+            i += 1;
         }
-        false
+        closest as i32
     }
 }
 
 fn main() {
-    assert!(!Solution::is_interleave(
-        "abaaacbacaab".to_string(),
-        "bcccababccc".to_string(),
-        "bcccabaaaaabccaccbacabb".to_string()
-    ));
-    assert!(!Solution::is_interleave(
-        "".to_string(),
-        "abc".to_string(),
-        "abcd".to_string()
-    ));
-    assert!(Solution::is_interleave(
-        "aabcc".to_string(),
-        "dbbca".to_string(),
-        "aadbbcbcac".to_string()
-    ));
-    assert!(!Solution::is_interleave(
-        "aabcc".to_string(),
-        "dbbca".to_string(),
-        "aadbbbaccc".to_string()
-    ));
-    assert!(Solution::is_interleave(
-        "".to_string(),
-        "".to_string(),
-        "".to_string()
-    ));
+    assert_eq!(-2, Solution::three_sum_closest(vec![-3, -2, -5, 3, -4], -1));
+    assert_eq!(2, Solution::three_sum_closest(vec![-1, 2, 1, -4], 1));
+    assert_eq!(0, Solution::three_sum_closest(vec![0, 0, 0], 1))
 }
