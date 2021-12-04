3ef7108337f38342a858063d9e824f24f0029c90
luo.fucong <luo.fucong@immomo.com>
2021-12-05 01:03:45 +0800

    https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,55 +3,46 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
-        assert!(nums.len() >= 3);
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        if s.len() == 0 {
+            return 0;
+        }
+        let chars = s.chars().collect::<Vec<char>>();
 
-        let nums = &mut { nums };
-        nums.sort();
+        use std::collections::HashSet;
+        let mut seen = HashSet::new();
+        seen.insert(chars[0]);
 
-        let mut closest: i64 = i32::MAX as i64;
-        let target = target as i64;
+        let mut longest = 1;
+
         let mut i = 0;
-        while i < nums.len() {
-            if i > 0 && nums[i] == nums[i - 1] {
-                i += 1;
-                continue;
-            }
-
-            let mut j = i + 1;
-            while j < nums.len() {
-                if j > i + 1 && nums[j] == nums[j - 1] {
-                    j += 1;
-                    continue;
-                }
-
-                let mut k = j + 1;
-                while k < nums.len() {
-                    if k > j + 1 && nums[k] == nums[k - 1] {
-                        k += 1;
-                        continue;
-                    }
-
-                    let sum = nums[i] as i64 + nums[j] as i64 + nums[k] as i64;
-                    let d = (sum - target).abs();
-                    if d == 0 {
-                        return sum as i32;
-                    }
-                    if d < (closest - target).abs() {
-                        closest = sum;
-                    }
-                    k += 1;
-                }
+        let mut j = 1;
+        while j < chars.len() {
+            while j < chars.len() && seen.insert(chars[j]) {
                 j += 1;
             }
+            longest = std::cmp::max(longest, j - i);
+            seen.remove(&chars[i]);
             i += 1;
         }
-        closest as i32
+        longest as i32
     }
 }
 
 fn main() {
-    assert_eq!(-2, Solution::three_sum_closest(vec![-3, -2, -5, 3, -4], -1));
-    assert_eq!(2, Solution::three_sum_closest(vec![-1, 2, 1, -4], 1));
-    assert_eq!(0, Solution::three_sum_closest(vec![0, 0, 0], 1))
+    assert_eq!(3, Solution::length_of_longest_substring("bwf".to_string()));
+    assert_eq!(1, Solution::length_of_longest_substring(" ".to_string()));
+    assert_eq!(
+        3,
+        Solution::length_of_longest_substring("abcabcbb".to_string())
+    );
+    assert_eq!(
+        1,
+        Solution::length_of_longest_substring("bbbbb".to_string())
+    );
+    assert_eq!(
+        3,
+        Solution::length_of_longest_substring("pwwkew".to_string())
+    );
+    assert_eq!(0, Solution::length_of_longest_substring("".to_string()));
 }
