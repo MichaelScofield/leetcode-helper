77527e6dc7e7a097894e7ddc6206f25794dc0dea
luo.fucong <luo.fucong@immomo.com>
2021-12-11 20:14:15 +0800

    https://leetcode-cn.com/problems/first-missing-positive/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,46 +3,43 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {
-        if s.len() == 0 {
-            return 0;
-        }
-        let chars = s.chars().collect::<Vec<char>>();
+    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
+        assert!(nums.len() >= 1);
+        let nums = &mut { nums };
 
-        use std::collections::HashSet;
-        let mut seen = HashSet::new();
-        seen.insert(chars[0]);
-
-        let mut longest = 1;
-
-        let mut i = 0;
-        let mut j = 1;
-        while j < chars.len() {
-            while j < chars.len() && seen.insert(chars[j]) {
-                j += 1;
+        for num in nums.iter_mut() {
+            if *num <= 0 {
+                *num = i32::MAX;
             }
-            longest = std::cmp::max(longest, j - i);
-            seen.remove(&chars[i]);
-            i += 1;
         }
-        longest as i32
+
+        for i in 0..nums.len() {
+            if nums[i] == i32::MAX {
+                continue;
+            }
+
+            let j = (nums[i].abs() - 1) as usize;
+            if j < nums.len() {
+                if nums[j] > 0 {
+                    nums[j] = -nums[j];
+                }
+            }
+        }
+
+        for i in 0..nums.len() {
+            if nums[i] > 0 {
+                return (i + 1) as i32;
+            }
+        }
+        (nums.len() + 1) as i32
     }
 }
 
 fn main() {
-    assert_eq!(3, Solution::length_of_longest_substring("bwf".to_string()));
-    assert_eq!(1, Solution::length_of_longest_substring(" ".to_string()));
-    assert_eq!(
-        3,
-        Solution::length_of_longest_substring("abcabcbb".to_string())
-    );
-    assert_eq!(
-        1,
-        Solution::length_of_longest_substring("bbbbb".to_string())
-    );
-    assert_eq!(
-        3,
-        Solution::length_of_longest_substring("pwwkew".to_string())
-    );
-    assert_eq!(0, Solution::length_of_longest_substring("".to_string()));
+    assert_eq!(3, Solution::first_missing_positive(vec![1, 2, 0]));
+    assert_eq!(2, Solution::first_missing_positive(vec![1, 1]));
+    assert_eq!(4, Solution::first_missing_positive(vec![1, 2, 3]));
+    assert_eq!(1, Solution::first_missing_positive(vec![4, 2, 3]));
+    assert_eq!(2, Solution::first_missing_positive(vec![3, 4, -1, 1]));
+    assert_eq!(1, Solution::first_missing_positive(vec![7, 8, 9, 11, 12]));
 }
