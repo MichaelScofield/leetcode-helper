5731a496e8697e85ed06230eb486f70e91e5207a
luo.fucong <luo.fucong@immomo.com>
2021-12-19 15:31:14 +0800

    https://leetcode-cn.com/problems/generate-parentheses/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,57 +3,47 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
-        assert!(nums.len() >= 3);
-
-        let nums = &mut { nums };
-        nums.sort();
-
-        let mut closest: i64 = i32::MAX as i64;
-        let target = target as i64;
-        let mut i = 0;
-        while i < nums.len() {
-            if i > 0 && nums[i] == nums[i - 1] {
-                i += 1;
-                continue;
-            }
-
-            let mut j = i + 1;
-            let mut k = nums.len() - 1;
-            while j < k {
-                if j > i + 1 && nums[j] == nums[j - 1] {
-                    j += 1;
-                    continue;
-                }
-                if k < nums.len() - 1 && nums[k] == nums[k + 1] {
-                    k -= 1;
-                    continue;
-                }
-
-                let sum = nums[i] as i64 + nums[j] as i64 + nums[k] as i64;
-                if sum == target {
-                    return sum as i32;
-                } else {
-                    if sum < target {
-                        j += 1;
-                    } else {
-                        k -= 1;
-                    }
-
-                    if (sum - target).abs() < (closest - target).abs() {
-                        closest = sum;
-                    }
-                }
-            }
-            i += 1;
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        if n < 1 {
+            return Vec::with_capacity(0);
         }
-        closest as i32
+
+        use std::collections::HashSet;
+        fn generate(n: usize) -> HashSet<Vec<char>> {
+            if n == 1 {
+                let mut parenthesis = HashSet::with_capacity(1);
+                parenthesis.insert(vec!['(', ')']);
+                return parenthesis;
+            }
+
+            let mut set = HashSet::new();
+            let v = generate(n - 1);
+            let mut tmp = Vec::<char>::with_capacity(n * 2);
+            for parenthesis in v {
+                for i in 0..parenthesis.len() {
+                    tmp.push('(');
+                    tmp.extend_from_slice(&parenthesis[0..i]);
+                    tmp.push(')');
+                    tmp.extend_from_slice(&parenthesis[i..]);
+
+                    set.insert(tmp.clone());
+                    tmp.clear();
+                }
+            }
+            set
+        }
+
+        let parenthesis = generate(n as usize);
+        use std::iter::FromIterator;
+        parenthesis
+            .into_iter()
+            .map(|x| String::from_iter(x.iter()))
+            .collect()
     }
 }
 
 fn main() {
-    assert_eq!(2, Solution::three_sum_closest(vec![-1, 0, 1, 1, 55], 3));
-    assert_eq!(-2, Solution::three_sum_closest(vec![-3, -2, -5, 3, -4], -1));
-    assert_eq!(2, Solution::three_sum_closest(vec![-1, 2, 1, -4], 1));
-    assert_eq!(0, Solution::three_sum_closest(vec![0, 0, 0], 1))
+    let solution = Solution::generate_parenthesis(3);
+    println!("solution = {:?}", solution);
+    assert_eq!(5, solution.len());
 }
