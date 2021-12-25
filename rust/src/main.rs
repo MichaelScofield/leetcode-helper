cf7463ec815e533b62a59f07dafca291c375516a
luo.fucong <luo.fucong@immomo.com>
2021-12-25 22:12:24 +0800

    https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,47 +1,49 @@
+use crate::helper::def::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 mod helper;
 
 struct Solution;
 
 impl Solution {
-    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        if nums.len() == 0 {
-            return 0;
-        }
+    pub fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        fn traversal(node: Option<Rc<RefCell<TreeNode>>>, path: &mut Vec<i32>, sum: &mut i32) {
+            if let Some(node) = node {
+                path.push(node.borrow().val);
 
-        use std::collections::HashSet;
-        let mut set = HashSet::new();
-        for num in nums {
-            set.insert(num);
-        }
-
-        let mut sequence_len = 1;
-        let mut longest_sequence_len = 1;
-        for &num in set.iter() {
-            if !set.contains(&(num - 1)) {
-                sequence_len = 1;
-                let mut i = num + 1;
-                while set.contains(&i) {
-                    sequence_len += 1;
-                    i += 1;
+                let mut is_leaf = true;
+                if let Some(left) = node.borrow().left.clone() {
+                    is_leaf = false;
+                    traversal(Some(left), path, sum);
                 }
-                longest_sequence_len = std::cmp::max(longest_sequence_len, sequence_len);
+                if let Some(right) = node.borrow().right.clone() {
+                    is_leaf = false;
+                    traversal(Some(right), path, sum);
+                }
+                if is_leaf {
+                    *sum += path
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .fold(0, |acc, (i, &n)| acc + n * 10i32.pow(i as u32));
+                }
+
+                path.pop();
             }
         }
-        std::cmp::max(longest_sequence_len, sequence_len)
+        let mut path = vec![];
+        let mut sum = 0;
+        traversal(root, &mut path, &mut sum);
+        sum
     }
 }
 
 fn main() {
+    use helper::util::create_binary_tree;
+    assert_eq!(25, Solution::sum_numbers(create_binary_tree(vec![1, 2, 3])));
     assert_eq!(
-        10,
-        Solution::longest_consecutive(vec![
-            0, 1, 2, 4, 8, 5, 6, 7, 9, 3, 55, 88, 77, 99, 999999999
-        ])
-    );
-    assert_eq!(1, Solution::longest_consecutive(vec![1]));
-    assert_eq!(4, Solution::longest_consecutive(vec![100, 4, 200, 1, 3, 2]));
-    assert_eq!(
-        9,
-        Solution::longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1])
+        1026,
+        Solution::sum_numbers(create_binary_tree(vec![4, 9, 0, 5, 1]))
     );
 }
