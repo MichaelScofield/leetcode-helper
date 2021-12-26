53c89230b2abbb0f32673b9c4e4af75983d760f6
luo.fucong <luo.fucong@immomo.com>
2021-12-26 17:50:57 +0800

    https://leetcode-cn.com/problems/palindrome-partitioning/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,211 +3,92 @@ mod helper;
 struct Solution;
 
 impl Solution {
-    pub fn solve(board: &mut Vec<Vec<char>>) {
-        let m = board.len();
-        assert!(m >= 1);
-        let n = board[0].len();
-        assert!(n >= 1);
+    pub fn partition(s: String) -> Vec<Vec<String>> {
+        assert!(s.len() >= 1);
 
-        let mut taints = vec![];
-        for i in 0..m {
-            if board[i][0] == 'O' {
-                board[i][0] = ' ';
-                taints.push((i, 0));
+        use std::collections::HashMap;
+        use std::collections::HashSet;
+        fn partition0<'a>(
+            s: &'a [u8],
+            cache: &mut HashMap<&'a [u8], HashSet<Vec<String>>>,
+        ) -> HashSet<Vec<String>> {
+            if let Some(ans) = cache.get(s) {
+                return ans.clone();
             }
-            if board[i][n - 1] == 'O' {
-                board[i][n - 1] = ' ';
-                taints.push((i, n - 1));
-            }
-        }
-        for j in 0..n {
-            if board[0][j] == 'O' {
-                board[0][j] = ' ';
-                taints.push((0, j));
-            }
-            if board[m - 1][j] == 'O' {
-                board[m - 1][j] = ' ';
-                taints.push((m - 1, j));
-            }
-        }
+            let mut ans = HashSet::new();
+            let mut i = 0;
+            while i < s.len() {
+                let mut palindromes = Vec::with_capacity(2);
+                let mut l = i;
+                let mut r = i;
+                while l >= 1 && r <= s.len() - 2 && s[l - 1] == s[r + 1] {
+                    l -= 1;
+                    r += 1;
+                }
+                palindromes.push((l, r));
 
-        while let Some((i, j)) = taints.pop() {
-            if i > 0 {
-                if board[i - 1][j] == 'O' {
-                    board[i - 1][j] = ' ';
-                    taints.push((i - 1, j));
+                if i > 0 && s[i - 1] == s[i] {
+                    let mut l = i - 1;
+                    let mut r = i;
+                    while l >= 1 && r <= s.len() - 2 && s[l - 1] == s[r + 1] {
+                        l -= 1;
+                        r += 1;
+                    }
+                    palindromes.push((l, r));
                 }
-            }
-            if i < m - 1 {
-                if board[i + 1][j] == 'O' {
-                    board[i + 1][j] = ' ';
-                    taints.push((i + 1, j));
-                }
-            }
-            if j > 0 {
-                if board[i][j - 1] == 'O' {
-                    board[i][j - 1] = ' ';
-                    taints.push((i, j - 1));
-                }
-            }
-            if j < n - 1 {
-                if board[i][j + 1] == 'O' {
-                    board[i][j + 1] = ' ';
-                    taints.push((i, j + 1));
-                }
-            }
-        }
 
-        for i in 1..m - 1 {
-            for j in 1..n - 1 {
-                if board[i][j] == 'O' {
-                    board[i][j] = 'X';
+                for (l, r) in palindromes {
+                    let left_parts = partition0(&s[0..l], cache);
+                    let right_parts = partition0(&s[r + 1..], cache);
+                    if !left_parts.is_empty() {
+                        if !right_parts.is_empty() {
+                            for lp in left_parts.iter() {
+                                for rp in right_parts.iter() {
+                                    let mut v = vec![];
+                                    v.append(&mut lp.clone());
+                                    v.push(String::from_utf8(s[l..=r].to_vec()).unwrap());
+                                    v.append(&mut rp.clone());
+                                    ans.insert(v);
+                                }
+                            }
+                        } else {
+                            for lp in right_parts.iter() {
+                                let mut v = vec![];
+                                v.append(&mut lp.clone());
+                                v.push(String::from_utf8(s[l..=r].to_vec()).unwrap());
+                                ans.insert(v);
+                            }
+                        }
+                    } else {
+                        if !right_parts.is_empty() {
+                            for rp in right_parts.iter() {
+                                let mut v = vec![];
+                                v.push(String::from_utf8(s[l..=r].to_vec()).unwrap());
+                                v.append(&mut rp.clone());
+                                ans.insert(v);
+                            }
+                        } else {
+                            let mut v = vec![];
+                            v.push(String::from_utf8(s[l..=r].to_vec()).unwrap());
+                            ans.insert(v);
+                        }
+                    }
                 }
-            }
-        }
 
-        for i in 0..m {
-            for j in 0..n {
-                if board[i][j] == ' ' {
-                    board[i][j] = 'O';
-                }
+                i += 1;
             }
+            cache.insert(s, ans.clone());
+            ans
         }
+        let mut cache = HashMap::new();
+        partition0(s.as_bytes(), &mut cache).into_iter().collect()
     }
 }
 
 fn main() {
-    use helper::util::cast;
-    use helper::util::print_matrix;
-
-    let mut board = cast(vecvec![
-        [
-            "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
-            "X", "X", "X"
-        ],
-        [
-            "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "O", "O", "X", "X", "X", "X", "X",
-            "X", "X", "X"
-        ],
-        [
-            "X", "X", "X", "X", "X", "O", "O", "O", "X", "O", "X", "O", "X", "X", "X", "X", "X",
-            "X", "X", "X"
-        ],
-        [
-            "X", "X", "X", "X", "X", "O", "X", "O", "X", "O", "X", "O", "O", "O", "X", "X", "X",
-            "X", "X", "X"
-        ],
-        [
-            "X", "X", "X", "X", "X", "O", "X", "O", "O", "O", "X", "X", "X", "X", "X", "X", "X",
-            "X", "X", "X"
-        ],
-        [
-            "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
-            "X", "X", "X"
-        ]
-    ]);
-    Solution::solve(&mut board);
-    print_matrix(&board);
-    assert_eq!(
-        cast(vecvec![
-            [
-                "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
-                "X", "X", "X", "X"
-            ],
-            [
-                "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "O", "O", "X", "X", "X", "X",
-                "X", "X", "X", "X"
-            ],
-            [
-                "X", "X", "X", "X", "X", "O", "O", "O", "X", "O", "X", "O", "X", "X", "X", "X",
-                "X", "X", "X", "X"
-            ],
-            [
-                "X", "X", "X", "X", "X", "O", "X", "O", "X", "O", "X", "O", "O", "O", "X", "X",
-                "X", "X", "X", "X"
-            ],
-            [
-                "X", "X", "X", "X", "X", "O", "X", "O", "O", "O", "X", "X", "X", "X", "X", "X",
-                "X", "X", "X", "X"
-            ],
-            [
-                "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",
-                "X", "X", "X", "X"
-            ]
-        ]),
-        board
-    );
-
-    let mut board = cast(vecvec![
-        ["X", "O", "X", "O", "X", "O", "O", "O", "X", "O"],
-        ["X", "O", "O", "X", "X", "X", "O", "O", "O", "X"],
-        ["O", "O", "O", "O", "O", "O", "O", "O", "X", "X"],
-        ["O", "O", "O", "O", "O", "O", "X", "O", "O", "X"],
-        ["O", "O", "X", "X", "O", "X", "X", "O", "O", "O"],
-        ["X", "O", "O", "X", "X", "X", "O", "X", "X", "O"],
-        ["X", "O", "X", "O", "O", "X", "X", "O", "X", "O"],
-        ["X", "X", "O", "X", "X", "O", "X", "O", "O", "X"],
-        ["O", "O", "O", "O", "X", "O", "X", "O", "X", "O"],
-        ["X", "X", "O", "X", "X", "X", "X", "O", "O", "O"]
-    ]);
-    Solution::solve(&mut board);
-    assert_eq!(
-        vecvec![
-            ['X', 'O', 'X', 'O', 'X', 'O', 'O', 'O', 'X', 'O'],
-            ['X', 'O', 'O', 'X', 'X', 'X', 'O', 'O', 'O', 'X'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
-            ['O', 'O', 'O', 'O', 'O', 'O', 'X', 'O', 'O', 'X'],
-            ['O', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'O', 'O'],
-            ['X', 'O', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
-            ['X', 'O', 'X', 'X', 'X', 'X', 'X', 'O', 'X', 'O'],
-            ['X', 'X', 'O', 'X', 'X', 'X', 'X', 'O', 'O', 'X'],
-            ['O', 'O', 'O', 'O', 'X', 'X', 'X', 'O', 'X', 'O'],
-            ['X', 'X', 'O', 'X', 'X', 'X', 'X', 'O', 'O', 'O']
-        ],
-        board
-    );
-
-    let mut board = cast(vecvec![
-        ["O", "X", "X", "O", "X"],
-        ["X", "O", "O", "X", "O"],
-        ["X", "O", "X", "O", "X"],
-        ["O", "X", "O", "O", "O"],
-        ["X", "X", "O", "X", "O"]
-    ]);
-    Solution::solve(&mut board);
-    assert_eq!(
-        vecvec![
-            ['O', 'X', 'X', 'O', 'X'],
-            ['X', 'X', 'X', 'X', 'O'],
-            ['X', 'X', 'X', 'O', 'X'],
-            ['O', 'X', 'O', 'O', 'O'],
-            ['X', 'X', 'O', 'X', 'O']
-        ],
-        board
-    );
-
-    let mut board = cast(vecvec![
-        ["X", "X", "X", "X"],
-        ["X", "O", "O", "X"],
-        ["X", "X", "O", "X"],
-        ["X", "O", "X", "X"]
-    ]);
-    Solution::solve(&mut board);
-    assert_eq!(
-        vecvec![
-            ['X', 'X', 'X', 'X'],
-            ['X', 'X', 'X', 'X'],
-            ['X', 'X', 'X', 'X'],
-            ['X', 'O', 'X', 'X']
-        ],
-        board
-    );
-
-    let mut board = cast(vecvec![["X"]]);
-    Solution::solve(&mut board);
-    assert_eq!(vecvec![['X']], board);
-
-    let mut board = cast(vecvec![["O"]]);
-    Solution::solve(&mut board);
-    assert_eq!(vecvec![['O']], board);
+    println!("{:?}", Solution::partition("fffffffffffffff".to_string()));
+    println!("{:?}", Solution::partition("aabaa".to_string()));
+    println!("{:?}", Solution::partition("aba".to_string()));
+    println!("{:?}", Solution::partition("aab".to_string()));
+    assert_eq!(vecvec![["a"]], Solution::partition("a".to_string()));
 }
