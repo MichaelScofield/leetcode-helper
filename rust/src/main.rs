4fe62526543ae44a7136ee6db7ac414955183013
luo.fucong <luo.fucong@immomo.com>
2022-01-15 23:22:02 +0800

    https://leetcode-cn.com/problems/implement-trie-prefix-tree/

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,32 +1,82 @@
 mod helper;
 
-struct Solution;
+#[derive(Default)]
+struct Trie {
+    children: [Option<Box<Trie>>; 26],
+    is_word_ending: bool,
+}
 
-impl Solution {
-    pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
-        assert!(dungeon.len() >= 1);
-        let m = dungeon.len();
-        assert!(dungeon[0].len() >= 1);
-        let n = dungeon[0].len();
+impl Trie {
+    fn new() -> Self {
+        Default::default()
+    }
 
-        let mut dp = vec![vec![i32::MAX; n + 1]; m + 1];
-        dp[m - 1][n] = 1;
-        dp[m][n - 1] = 1;
-        for i in (0..m).rev() {
-            for j in (0..n).rev() {
-                dp[i][j] =
-                    std::cmp::max(std::cmp::min(dp[i][j + 1], dp[i + 1][j]) - dungeon[i][j], 1);
+    fn insert(&mut self, word: String) {
+        assert!(word.len() >= 1);
+
+        let mut trie = self;
+        for ch in word.chars() {
+            trie = trie.children[(ch as usize - 'a' as usize)]
+                .get_or_insert_with(|| Box::new(Trie::new()));
+        }
+        trie.is_word_ending = true
+    }
+
+    fn search(&self, word: String) -> bool {
+        assert!(word.len() >= 1);
+
+        if let Some(trie) = self.search0(word) {
+            trie.is_word_ending
+        } else {
+            false
+        }
+    }
+
+    fn starts_with(&self, prefix: String) -> bool {
+        assert!(prefix.len() >= 1);
+
+        self.search0(prefix).is_some()
+    }
+
+    fn search0(&self, word: String) -> Option<&Trie> {
+        assert!(word.len() >= 1);
+
+        let mut trie = self;
+        for ch in word.chars() {
+            if let Some(x) = trie.children[(ch as usize - 'a' as usize)].as_ref() {
+                trie = x;
+            } else {
+                return None;
             }
         }
-        dp[0][0]
+        Some(trie)
     }
 }
 
 fn main() {
-    assert_eq!(4, Solution::calculate_minimum_hp(vecvec![[-3, 5]]));
-    assert_eq!(1, Solution::calculate_minimum_hp(vecvec![[0, 0]]));
-    assert_eq!(
-        7,
-        Solution::calculate_minimum_hp(vecvec![[-2, -3, 3], [-5, -10, 1], [10, 30, -5]])
-    );
+    let mut trie = Trie::new();
+    trie.insert("app".to_string());
+    trie.insert("apple".to_string());
+    trie.insert("beer".to_string());
+    trie.insert("add".to_string());
+    trie.insert("jam".to_string());
+    trie.insert("rental".to_string());
+    assert!(!trie.search("apps".to_string()));
+    assert!(trie.search("app".to_string()));
+    assert!(!trie.search("ad".to_string()));
+    assert!(!trie.search("applepie".to_string()));
+    assert!(!trie.search("rest".to_string()));
+    assert!(!trie.search("jan".to_string()));
+    assert!(!trie.search("rent".to_string()));
+    assert!(trie.search("beer".to_string()));
+    assert!(trie.search("jam".to_string()));
+    assert!(!trie.starts_with("apps".to_string()));
+    assert!(trie.starts_with("app".to_string()));
+    assert!(trie.starts_with("ad".to_string()));
+    assert!(!trie.starts_with("applepie".to_string()));
+    assert!(!trie.starts_with("rest".to_string()));
+    assert!(!trie.starts_with("jan".to_string()));
+    assert!(trie.starts_with("rent".to_string()));
+    assert!(trie.starts_with("beer".to_string()));
+    assert!(trie.starts_with("jam".to_string()));
 }
